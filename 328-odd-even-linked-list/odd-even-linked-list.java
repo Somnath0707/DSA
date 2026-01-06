8506502d6b5d2a8a95790183df37a257e5bb55c5
Som_0707 <149756531+Somnath0707@users.noreply.github.com>
2026-01-06 08:22:11 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (5.27%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -10,23 +10,40 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
+
         if(head == null){
             return null;
         }
-        ListNode odd = head; 
-        ListNode even = head.next;
+        // Start at the head of List as its no 1 and that is odd 
+        ListNode odd = head ;
+        // Start at the head.next that is 2 no which is even 
+        ListNode even = head.next; 
 
+        // We need to connect the last of odd to evenfirst so keep this as Head of even nodes
         ListNode evenHead = even;
 
+
+        // Run this loop till the even goes out of the list 
         while(even != null && even.next != null){
-            odd.next = odd.next.next;
+
+            // So we know the odd are 1, 3, 4 so we know its odd.next.next 
+            odd.next = odd.next.next; 
+            odd = odd.next; 
+
+
+            // We know for the even 2, 4, 6 so we know to do same 
             even.next = even.next.next;
-
-            odd = odd.next;
-            even = even.next;
+            even = even.next ;
         }
+        // After this the odd will be to the last element of odd 
+        // even will be out of park that is null 
 
-        odd.next  = evenHead;
+        // The list will look like 
+        // 1->3->5   2->4->6
+        //       o   eH         connect the odd (0) to the evenHead(eH)
+        odd.next = evenHead;
+
+        // Return Normal head
         return head; 
     }
 }
