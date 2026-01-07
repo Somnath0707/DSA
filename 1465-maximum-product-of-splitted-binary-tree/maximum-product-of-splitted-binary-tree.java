9c4657524298658c8cd91119e08b6b815d01cce8
Som_0707 <149756531+Somnath0707@users.noreply.github.com>
2026-01-07 21:35:53 +0530

    Time: 8 ms (32.68%) | Memory: 61 MB (22.57%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
@@ -1,46 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    long MOD = 1000000007L;
-    long ans = 0;
-
-    private long dfs(TreeNode node) {
-        if (node == null) return 0;
-
-        node.val += dfs(node.left) + dfs(node.right);
-        return node.val;
-    }
-
+    long maxP=0, SUM=0;
     public int maxProduct(TreeNode root) {
-        long total = dfs(root);
+        if (root == null) return 0;
 
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
+        //long maxP=0, SUM=0;
+        SUM= totalsum(root);
+        totalsum(root);
 
-        while (!q.isEmpty()) {
-            TreeNode node = q.poll();
-            if (node == null) continue;
-
-            long cur = (total - node.val) * node.val;
-            ans = Math.max(ans, cur);
-
-            if (node.left != null) q.add(node.left);
-            if (node.right != null) q.add(node.right);
-        }
-
-        return (int)(ans % MOD);
+        return (int)(maxP % 1000000007);
     }
-}
+
+         int totalsum(TreeNode root)
+        {
+                if(root==null) return 0;
+
+                int lss= totalsum(root.left);
+                int rss= totalsum(root.right);
+                int subts= root.val+ lss+ rss;
+
+                maxP = Math.max(maxP, subts * (SUM - subts));
+                return subts;
+        }
+        //return (int)(maxP % 1000000007);
+    }
