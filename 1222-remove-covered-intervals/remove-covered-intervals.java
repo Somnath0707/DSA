9a6e55f5b5228d79d7cf03c2e2fc6de9b13223c1
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-01-11 14:52:35 +0530

    Time: 6 ms (91.96%) | Memory: 46.6 MB (54.97%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.java
+++ 1222-remove-covered-intervals/remove-covered-intervals.java
@@ -1,6 +1,39 @@
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
-        // [1,4] [2,8] [3,6]
+
+    // Sort the array in such a way that the Start should be ascending and end by decending 
+        // You might be like: "wtf does this mean?" 😄
+        // Relax. Why fear when I am here.
+
+        // Example:
+        // [[1,2], [3,4], [1,4]]
+
+        // If we sort ONLY by start (ascending):
+        // Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+
+        // Result becomes:
+        // [[1,2], [1,4], [3,4]]
+
+        // ❌ Problem:
+        // Both [1,2] and [1,4] start at 1,
+        // but the smaller interval [1,2] comes first.
+        // This can break logic in problems like:
+        // - Merge Intervals
+        // - Remove Covered Intervals
+        // - Interval Scheduling
+
+        // ✅ Correct idea:
+        // If two intervals have the SAME start,
+        // the BIGGER interval should come first.
+
+        // So we:
+        // 1️⃣ Sort start in ascending order
+        // 2️⃣ If starts are equal, sort end in DESCENDING order
+
+        // Final correct order:
+        // [[1,4], [1,2], [3,4]]
+
+
         Arrays.sort(intervals, (a,b)->{
             if (a[0]!=b[0]){
                 return a[0] - b[0];// start ascending
@@ -8,16 +41,26 @@ class Solution {
             return b[1]-a[1]; // end descending 
         });
 
+        // Place last End to the first arrays end to compare to the Start of the next;
         int lastEnd = intervals[0][1];
+
+        //To keep the track of total 
         int total = intervals.length ; 
 
         for(int i = 1 ; i < intervals.length ; i ++){
             int Scurr = intervals[i][0];
             int Ecurr = intervals[i][1];
 
+
+            // Check if the next falls between the last 
+            // Example [[1,4], [1,2]] lastEnd = 4 , Scurr = 1 , Ecurr = 2;
+            // if 1<4 and 2 is also less than 4 so it is inside the the first array 
             if(lastEnd>= Scurr && lastEnd >= Ecurr){
+                // We decrease the total 
                 total--;
             }else{
+
+                // Else we know it is not inside the array and we go to next array 
                 lastEnd = Ecurr;
             }
         }
