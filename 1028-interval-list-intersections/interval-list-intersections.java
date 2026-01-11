7af897fbc1d7ccea2f61443792e353a1ebcb47e6
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-01-11 13:43:20 +0530

    Time: 4 ms (57.77%) | Memory: 47.4 MB (61.34%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.java
+++ 1028-interval-list-intersections/interval-list-intersections.java
@@ -1,22 +1,9 @@
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-        
+        List<int []> list= new ArrayList<>();
+        int i = 0 , j = 0 ; 
 
-        // We have to get the numbers which are in the interval even if one 
-
-        // We have to compare between two the first list and second list 
-
-        // 0..1..2        5..6..7..8..9..10      
-        //    1..2..3..4..5        8..9..10..12          
-
-        // [1,2][5,5][8,10]
-
-        List<int[]> list = new ArrayList<>();
-        int i = 0, j =0 ; 
-        
-        while(i < firstList.length && j < secondList.length){
-            // Usual overlap condition E1>=S2 && E2>=S1
-
+        while(i< firstList.length && j < secondList.length){
             int s1 = firstList[i][0];
             int s2 = secondList[j][0];
 
@@ -24,23 +11,26 @@ class Solution {
             int e2 = secondList[j][1];
 
             if(e1>=s2 && e2>=s1){
-                list.add(new int[] {
-                    Math.max(s1,s2),Math.min(e1,e2)
+                list.add(new int[]{
+                    Math.max(s1,s2),
+                    Math.min(e1,e2)
                 });
             }
-            if(e1 < e2){
-                i =i +1;
-            }else{
-                j =j+ 1;
-            }
 
+            if(e1<e2){
+                i++;
+            }
+            else{
+                j++;
+            }
         }
 
-        int[][] ans = new int[list.size()][2];
+        int[][] res = new int[list.size()][2];
 
-            for (int k =0 ; k <list.size() ; k++){
-                ans[k] = list.get(k);
-            }
-            return ans ; 
+        for(int k =0 ; k < list.size(); k++){
+            res[k] = list.get(k);
+        }
+
+        return res ; 
     }
 }
