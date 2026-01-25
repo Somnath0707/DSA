77e61a8b0435dbb2ac4495ab6447d8f5d26d19ce
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-01-25 21:08:38 +0530

    Time: 3 ms (37.87%) | Memory: 46.8 MB (12.12%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+
+    public void connectNodes(Node left ,  Node right){
+        if(left == null || right == null){
+            return ;
+        }
+
+        left.next = right;
+        connectNodes(left.right, right.left);
+
+        connectNodes(left.left, left.right);
+        connectNodes(right.left, right.right);
+    }
+    public Node connect(Node root) {
+        if (root == null) return null;
+
+        connectNodes(root.left , root.right);
+
+        return root;
+    }
+}
