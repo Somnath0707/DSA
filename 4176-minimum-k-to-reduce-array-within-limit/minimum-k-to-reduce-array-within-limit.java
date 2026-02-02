99709410f53d2ad4f6bf036f92c81482ba1b476f
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-02-02 18:58:27 +0530

    Time: 66 ms (0.00%) | Memory: 124.1 MB (100.00%) - LeetSync

--- 4176-minimum-k-to-reduce-array-within-limit/minimum-k-to-reduce-array-within-limit.java
+++ 4176-minimum-k-to-reduce-array-within-limit/minimum-k-to-reduce-array-within-limit.java
@@ -7,9 +7,12 @@ class Solution {
         long left = 1; 
         long right = 100000; // take this as the upper constraint so that we can also do it 
 
+        // noraml binary search 
         while(left <= right){
+        // normal mid calculation 
         long mid = left + ( right - left ) /2 ;
 
+        // check the condition nonPositive(nums, k) <= k*k.
         if(getPos(nums , mid) <= mid * mid){
             right = mid - 1;
            }
@@ -17,13 +20,14 @@ class Solution {
            left = mid + 1; 
            }
         } 
-        return (int)
-        left ; 
+        // as the output is required in the int but then the output is long so we type cast 
+        return (int)left ; 
     }
 
     public long getPos(int[] nums , long mid){
         long operations = 0;
         for(int i =0 ; i < nums.length ; i++){
+            // get the ceil of the operations
             operations += ( nums[i] + mid - 1) / mid ; 
         }
 
