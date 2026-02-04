765bf5935ce5cb1ea0256e41e3a95aa8ca63beb7
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-02-04 19:17:16 +0530

    Time: 9 ms (59.61%) | Memory: 49.2 MB (28.89%) - LeetSync

--- 324-wiggle-sort-ii/wiggle-sort-ii.java
+++ 324-wiggle-sort-ii/wiggle-sort-ii.java
@@ -7,20 +7,30 @@ class Solution {
         // keep one pointer on the mid and the next pointer on the last and replace the elements in the og array with them 
         // if the i is odd take the low element which is from the mid if i is even take the one which from the rigth most 
 
+        // Sort the arrays 
         Arrays.sort(nums);
+
+        // Create a copy of the sorted array 
         int copy[] = Arrays.copyOf(nums , nums.length);
 
-
+        // Take the length for further simplification
         int n = nums.length;
-
+        // The mid to get the mid for the low elements from the left side of the copied array
         int mid = (n + 1) / 2;
 
+        // this conatins low elements than right 
         int indx1 = mid -1;
+        // This has all the high elements till mid 
         int indx2 = n-1;
+
+        // For loop to traverse over the nums array to replace them 
         for(int i = 0 ; i < n ; i ++){
+
+            // If the position is even we place the low side element 
             if(i % 2 == 0){
                 nums[i] = copy[indx1--];
             }
+            // If the position is odd we place the high side elements 
             if(i % 2 !=0){
                 nums[i] = copy[indx2--];
             }
