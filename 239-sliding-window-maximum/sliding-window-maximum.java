f1f3d768ca44ee74258f5786cd7a11c62dcca94f
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-02-11 23:45:30 +0530

    Time: 82 ms (9.79%) | Memory: 173.6 MB (5.05%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,29 +1,37 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        PriorityQueue<int []> q = new PriorityQueue<>((a,b) -> b[0] - a[0]);
 
-        for(int i =0 ; i< k; i++){
-            q.offer(new int[]{nums[i] , i});
+        // Create a priority Queue to store the element and the index for the window size
+
+        PriorityQueue<int[] > pq = new PriorityQueue<>((a,b) -> b[0] - a[0]);
+
+        // Put the value int queue till the window i.e the first window 
+        for(int i =0 ; i< k ; i++){
+            pq.offer(new int[]{nums[i] , i });
         }
 
-        List<Integer> list = new ArrayList<>();
-        int n = nums.length ; 
-        list.add(q.peek()[0]);
-        for(int i = k ; i< n ; i++){
-            q.offer(new int[]{nums[i] , i });
+        // Store the answer 
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        // Add the first max int the list 
+        list.add(pq.peek()[0]);
 
-            while(q.peek()[1] <= i -k  ){
-                q.poll();
+        for(int i =k ; i< nums.length  ; i++){
+            pq.offer(new int [] {nums[i] , i });
+
+
+            // We remove the value from the list which are out of the window 
+            while(pq.peek()[1] <= i-k){
+                pq.poll();
             }
-
-            list.add(q.peek()[0]);
+            list.add(pq.peek()[0]);
         }
         int ans [] = new int[list.size()];
 
-        for(int i = 0; i < list.size() ; i++){
+        for(int i =0 ; i< list.size() ; i++){
             ans[i] = list.get(i);
-        } 
+        }
+
         return ans ; 
-        
     }
 }
