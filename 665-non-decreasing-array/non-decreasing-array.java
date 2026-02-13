97c75911bcbfd09d2af113e91883c9e10f3e898a
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-02-13 20:39:48 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (80.36%) - LeetSync

--- 665-non-decreasing-array/non-decreasing-array.java
+++ 665-non-decreasing-array/non-decreasing-array.java
@@ -21,3 +21,15 @@ class Solution {
         return true;
     }
 }
+
+// [3,4,2,3]
+
+// is 3< 4 yes 
+// is 4<2 no count ++;
+//check is i here 0 no its 1 so we check is i-1 <= i+1 if so we know that left side has less than element so we change the value of the as the next element , cause if the previous was greater we would need to change it to that to make the next element fit 
+//.   i
+// [3,4,2,3] here 3 > 2 if we change 4 to 3 [3,3,2,3] but here there will be other that would be less than 
+// but if we do 4 to 2 
+
+// ts problem sucks fck it boi 
+
