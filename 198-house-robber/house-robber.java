a73f6b4b7c4dc3136a65d16e07a541f591a6a41f
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-02-16 15:08:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (88.20%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,17 +1,26 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length ;
-        int dp[] = new int[n];
+
+        // store the max answe till that time 
+        int ans = 0 ;
+
+        // if the length is 1 we can rob only that house 
         if(n == 1) return nums[0];
 
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0] , nums[1]);
+        // at 0 we can only rob that house 
+       int prev2 = nums[0];
+
+        // now next we could either rob 0 th house or the 1 th house and store the max in the 1 th in the array h
+        int prev = Math.max(nums[0] , nums[1]);
 
         for(int i = 2 ; i < n ; i++){
-            dp[i] = Math.max(nums[i] + dp[i-2] , dp[i-1]);
+           ans = Math.max(nums[i] + prev2,prev);
+           prev2 = prev;
+           prev = ans;
         }
 
-        return dp[n-1];
+        return prev;
         
     }
 }
