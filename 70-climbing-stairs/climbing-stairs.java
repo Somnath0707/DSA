e360762d62c9dd8064af7ff66dcff1a490a7f61a
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-02-16 11:31:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (54.16%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i =2 ; i< n+1 ; i++){
+            int left = dp[i-1];
+            int right = dp[i-2];
+
+            dp[i] = left+ right ;
+        }
+
+        return dp[n];
+    }
+}
