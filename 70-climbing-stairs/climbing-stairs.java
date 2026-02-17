1b70e18f295d60b0e1184551c23c06b0b4d004d9
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-02-17 18:36:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (73.46%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,19 +1,38 @@
 class Solution {
     public int climbStairs(int n) {
-        // create a dp array to keep track of the elements 
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return helper(n, dp);
-    }
+       int dp[] = new int[n+1];
 
-    public int helper( int n , int dp[]){
-        if(n<=1) return 1; 
+       dp[0] = 1; 
+       dp[1] = 1; 
 
-        if(dp[n] != -1) return dp[n];
+       for(int i = 2 ; i <= n ; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+       }
 
-        return dp[n] = helper(n-1, dp) + helper(n-2 , dp);
+       return dp[n];
     }
 }
+
+
+
+
+
+// class Solution {
+//     public int climbStairs(int n) {
+//         // create a dp array to keep track of the elements 
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp, -1);
+//         return helper(n, dp);
+//     }
+
+//     public int helper( int n , int dp[]){
+//         if(n<=1) return 1; 
+
+//         if(dp[n] != -1) return dp[n];
+
+//         return dp[n] = helper(n-1, dp) + helper(n-2 , dp);
+//     }
+// }
 
 
 
