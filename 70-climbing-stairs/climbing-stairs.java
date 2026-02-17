40572550dcf54c9d6083387b844933ed0c13ae07
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-02-17 18:31:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (54.55%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,16 +1,38 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[] = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
+        // create a dp array to keep track of the elements 
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return helper(n, dp);
+    }
 
-        for(int i =2 ; i< n+1 ; i++){
-            int left = dp[i-1];
-            int right = dp[i-2];
+    public int helper( int n , int dp[]){
+        if(n<=1) return 1; 
 
-            dp[i] = left+ right ;
-        }
+        if(dp[n] != -1) return dp[n];
 
-        return dp[n];
+        return dp[n] = helper(n-1, dp) + helper(n-2 , dp);
     }
 }
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int climbStairs(int n) {
+//         // base case 
+//         if(n == 0 || n ==1) return 1;
+
+//         // recursion 
+//         int left = climbStairs(n-1);
+//         int right = climbStairs(n-2);
+
+//         // total number of possible combinations
+//         return left + right;
+//     }
+// }
