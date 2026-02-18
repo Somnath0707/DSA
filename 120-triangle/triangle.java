283e001175c1ed96fd3ad3bc85e6904ce7d5f636
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-02-18 11:54:27 +0530

    Time: 90 ms (5.70%) | Memory: 45.5 MB (64.84%) - LeetSync

---
+++ 120-triangle/triangle.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> list) {
+        int total = list.size();
+        int dp[][] = new int[total][total];
+        for(int j =0 ; j< total ; j++){
+            dp[total-1][j] = list.get(total-1).get(j);
+            
+            for(int i = total-2 ; i>=0 ;i--){
+                int down = 0; 
+                int diagonal =0 ; 
+                for(int k = i ; k >=0 ; k--){
+                    down = list.get(i).get(k) + dp[i+1][k];
+                    diagonal = list.get(i).get(k) + dp[i+1][k+1];
+
+                    dp[i][k] = Math.min(down , diagonal);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
