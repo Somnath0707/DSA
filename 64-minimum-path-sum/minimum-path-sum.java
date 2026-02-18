5f3b75635b7ed7aafd4d1f485e523e8cc1eb5a06
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-02-18 08:43:29 +0530

    Time: 6 ms (10.56%) | Memory: 50.5 MB (21.27%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,23 +1,25 @@
 class Solution {
-    public int findPath(int row , int col , int[][] grid ,int dp[][] ){
-        if( row ==0 && col == 0 ) return grid[0][0];
-
-        if(row < 0 || col < 0) return 1000000000;
-
-        if(dp[row][col] != -1) return dp[row][col];
-
-        int up = grid[row][col] + findPath(row-1 , col , grid , dp);
-        int left = grid[row][col] + findPath(row,col-1 , grid , dp);
-        return dp[row][col] = Math.min(up , left);
-    }
     public int minPathSum(int[][] grid) {
         int row = grid.length ; 
         int col = grid[0].length ;
         int dp[][] = new int[row][col];
-        for(int i =0; i < row ; i++){
-            Arrays.fill(dp[i] , -1);
-        } 
 
-       return findPath(row-1 , col -1, grid,dp );
+        for(int i =0 ; i< row ; i++){
+            for(int j = 0 ; j < col ; j++){
+                if(i == 0 && j == 0 ) dp[i][j] = grid[0][0];
+                else{
+                    int left = 1000000000 ; 
+                    int up = 1000000000 ; 
+                    if(i > 0){
+                        up = grid[i][j] + dp[i-1][j];
+                    }
+                    if(j>0){
+                        left = grid[i][j] + dp[i][j-1];
+                    }
+                    dp[i][j] = Math.min(up , left);
+                }
+            }
+        }
+        return dp[row-1][col-1];
     }
 }
