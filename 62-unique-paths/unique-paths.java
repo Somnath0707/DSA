8efb816f1ec72648b7b50d5e52111560a50b134d
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-02-18 07:50:56 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (92.11%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,23 +1,22 @@
 class Solution {
-    public int findPath(int row , int col , int[][] dp){
-        if(row == 0 && col == 0 ) {
-            return 1;
-        }
-        if(row < 0 || col < 0 ) {
-            return 0 ; 
-        }
-        if(dp[row][col] != -1) return dp[row][col];
-
-        int up = findPath(row-1 , col, dp);
-        int left = findPath(row , col-1 , dp);
-
-        return dp[row][col]= up + left;
-    }
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
         for(int i =0 ; i< m ; i++){
-            Arrays.fill(dp[i], -1);
+            for(int j =0 ; j < n ; j++){
+                if(i==0 && j== 0 ) dp[0][0] = 1; 
+                else{
+                    int up = 0 ; 
+                    int left = 0;
+                    if(i>0){
+                        up = dp[i-1][j];
+                    }
+                    if(j>0) {
+                        left = dp[i][j-1];
+                    }
+                    dp[i][j] = up + left;
+                }
+            }
         }
-        return findPath(m-1 , n-1 , dp);
+        return dp[m-1][n-1];
     }
 }
