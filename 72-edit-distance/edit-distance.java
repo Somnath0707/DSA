377b9bbef42bc3497c85223afc82d46f6aa7ba04
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-02-23 13:55:31 +0530

    Time: 5 ms (67.10%) | Memory: 47.4 MB (33.26%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -16,10 +16,24 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
-        int dp[][] = new int [n][m];
-        for(int i =0 ; i < n ; i++){
-            Arrays.fill(dp[i] , -1 ) ;
+        int dp[][] = new int [n+1][m+1];
+        for(int i =0 ; i < n+1 ; i++){
+            dp[i][0] = i;
         }
-        return f(n-1 , m-1 , word1 , word2 , dp);
+
+        for(int j =0 ; j < m+1 ; j++) dp[0][j] = j;
+        for(int i = 1 ; i < n+1 ; i++){
+            for(int j = 1 ; j < m+1 ; j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
+
+                else {
+                    int delete = 1 + dp[i-1][j];
+                    int replace = 1 + dp[i-1][j-1] ;
+                    int insert = 1 + dp[i][j-1];
+                    dp[i][j] = Math.min(delete , Math.min(replace , insert));
+                }
+            }
+        }
+        return dp[n][m];
     }
 }
