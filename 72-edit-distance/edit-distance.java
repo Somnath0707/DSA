6938c03c5171d6c4ec817cdc67dfedbac6e647fa
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-02-24 07:30:45 +0530

    Time: 4 ms (76.73%) | Memory: 47.1 MB (71.79%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,39 +1,32 @@
 class Solution {
-    public int f(int i , int j  , String s1 , String s2 , int dp[][]){
-        if(i < 0 ) return j+1;
-        if(j<0) return i+1;
+    public int f(int i , int j  , String s1 , String s2 ,int [][] dp){
+        if(j<0) return i +1;
+        if(i<0) return j + 1;
+        
 
+        // if the character is same 
         if(s1.charAt(i) == s2.charAt(j)){
-            return f(i-1 , j-1 , s1 , s2  , dp);
+            return f(i-1 , j-1 , s1 , s2 , dp);
         }
-        if( dp[i][j]  != -1 ) return dp[i][j]  ; 
-        int delete = 1 + f(i-1 , j , s1 , s2 , dp);
-        int replace = 1 + f(i-1 , j-1 , s1 , s2 , dp);
-        int insert = 1 + f(i , j-1 , s1 , s2 , dp);
 
-        return dp[i][j] = Math.min(delete , Math.min(replace , insert));
+        if (dp[i][j] != -1 ) return dp[i][j];
+
+        int insert = 1 + f(i , j-1 , s1 , s2 , dp ) ;
+        int delete = 1 +  f(i-1 , j , s1 , s2 ,dp);
+        int replace = 1 +  f(i - 1 , j-1 , s1 , s2 ,dp);
+
+        return dp[i][j] = Math.min(insert , Math.min(delete , replace));
     }
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
-        int dp[][] = new int [n+1][m+1];
-        for(int i =0 ; i < n+1 ; i++){
-            dp[i][0] = i;
+
+        int [][] dp = new int [n][m];
+
+        for(int i =0 ; i< n ; i++){
+            Arrays.fill(dp[i] , -1);
         }
 
-        for(int j =0 ; j < m+1 ; j++) dp[0][j] = j;
-        for(int i = 1 ; i < n+1 ; i++){
-            for(int j = 1 ; j < m+1 ; j++){
-                if(word1.charAt(i-1) == word2.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
-
-                else {
-                    int delete = 1 + dp[i-1][j];
-                    int replace = 1 + dp[i-1][j-1] ;
-                    int insert = 1 + dp[i][j-1];
-                    dp[i][j] = Math.min(delete , Math.min(replace , insert));
-                }
-            }
-        }
-        return dp[n][m];
+        return f(n -1 , m - 1 , word1 , word2 ,dp);
     }
 }
