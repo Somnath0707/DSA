2f0da4497ffa379c412154274b2d38f38587b6ea
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-03-02 20:49:08 +0530

    Time: 25 ms (20.14%) | Memory: 47.2 MB (23.00%) - LeetSync

---
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String reverseParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            if(ch == ')'){
+                StringBuilder temp = new StringBuilder();
+
+                while(!stack.isEmpty() && stack.peek() != '('){
+                    temp.append(stack.pop());
+                }
+
+                // pop the (
+                stack.pop();
+
+                for(char c : temp.toString().toCharArray()){
+                    stack.push(c);
+                }
+            }
+            else{
+                stack.push(ch);
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        while(!stack.isEmpty()) {
+            ans.append(stack.pop());
+        }
+        return ans.reverse().toString();
+    }
+}
