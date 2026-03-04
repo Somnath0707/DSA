2d2765ae0d8d3bb3fbb62153d5165f82d6cc8197
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-03-04 12:18:52 +0530

    Time: 14 ms (74.16%) | Memory: 49 MB (66.90%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -3,13 +3,19 @@ class Solution {
         int n = mat.length;
         int m = mat[0].length;
 
+
+        // keep the track of the distance 
         int dis[][] = new int[n][m];
 
+        // check which was visited 
         boolean vis[][] = new boolean [n][m];
 
+
+        //queue for the bfs 
         Queue<int[]> q = new LinkedList<>();
 
 
+        // place all the value of the 0s queue and mark them visited 
         for(int i = 0; i < n ; i++){
             for(int j = 0; j < m ; j++){
                 if(mat[i][j] == 0 ) {
@@ -19,27 +25,38 @@ class Solution {
             }
         }
 
-
+        // for the four operation like up , down , left , right 
         int[] dr = new int[]{-1 , 0 , 1 , 0};
         int[] dc = new int[]{0 , 1 , 0 , -1};
+
+        //remove untill the queue is empty 
         while(!q.isEmpty()){
+
+            // remove the first from the queue and check ther neioubhours
             int curr[] = q.poll();
+            // row 
             int r = curr[0];
+            //column 
             int c =curr[1];
 
+            // move for each direction like up down left and right 
             for(int k = 0 ; k < 4 ; k++){
                 int newR = r + dr[k];
                 int newC = c + dc[k];
 
+                // for each direction check the condition and if that cell is visited or not 
+                // if the cell is not visited update the distance and mark them as distace if the cell is not visited the distance will + 1 from the preovious 
                 if(newR >= 0 && newC >= 0 && newR < n && newC < m && !vis[newR][newC]){
 
                     dis[newR][newC] = dis[r][c] + 1 ; 
                     vis[newR][newC] = true; 
 
+                    // add the visited cell to the queue to check its neigoubhour 
                     q.add(new int[]{newR, newC});
                 }
             }
         }
+        // return the answer 
         return dis;
     }
 }
