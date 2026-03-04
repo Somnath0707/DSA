f49b003f228830775d490a0d4608e48c160a12b7
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-03-04 21:09:14 +0530

    Time: 1344 ms (7.77%) | Memory: 281.1 MB (30.10%) - LeetSync

--- 3456-find-the-maximum-length-of-a-good-subsequence-i/find-the-maximum-length-of-a-good-subsequence-i.java
+++ 3456-find-the-maximum-length-of-a-good-subsequence-i/find-the-maximum-length-of-a-good-subsequence-i.java
@@ -1,38 +1,50 @@
+// PS note : as we are taking prev as the -1 we need to make for it we use the prev + 1 every where 
+
 class Solution {
-    public int f(int i , int prev ,  int nums[] , int k , int dp[][][] ) {
-        if(i >= nums.length ) return 0 ; 
+    public int f(int i , int prev , int nums [] , int k , int dp[][][]) {
+        // base case would be if we go outside the length 
+        int n = nums.length; 
+        if ( i == n) return 0 ; 
 
-        if(dp[i][prev+1][k] != -1 ) return dp[i][prev+1][k] ; 
+
+        if(dp[i][prev+1][k] != -1 ) return dp[i][prev+1][k]; 
+        // if we have two option that are skip or take 
         
-        int skip = f( i +1 , prev , nums, k , dp ) ;
 
-        int take = 0;
+        int take = 0 ; 
+
+        // if we are taking the first element we know it has no prev so -1 
+        if( prev == -1 ) 
+        // update the prev with the curr i 
+        take = 1 + f(i + 1 , i , nums , k , dp) ; 
 
 
-        // if the first element so prev would be -1 
-        if(prev == -1  )
-        take = 1 + f(i + 1, i , nums , k , dp ); 
+        // now check for the case where prev and i are same so we do not need to check the k 
+        else if(nums[i] == nums[prev])
+        take = 1 + f(i +1 , i , nums , k , dp  ) ; 
 
+        // if the two are not matching we check for the k 
+        else if( k > 0 ) 
+        take = 1 + f(i +1 , i  , nums , k-1 , dp ) ; 
 
-        else if (nums[i] == nums[prev]){
-            take = 1 + f(i + 1, i , nums , k  , dp ) ; 
-        }
+        int skip = f(i + 1 , prev , nums , k , dp ) ; 
 
-        else if ( k > 0 ) {
-            take = 1 + f(i + 1 , i , nums , k -1 , dp ) ; 
-        }
-
-        return dp[i][prev+1][k] = Math.max(take , skip ) ; 
-        
+        return dp[i][prev+1][k] =  Math.max(skip , take ) ; 
     }
     public int maximumLength(int[] nums, int k) {
-        int n = nums.length ; 
-        int dp[][][] = new int[n][n+1][k+1];
-        for(int i = 0; i < nums.length ; i++){
-           for(int j = 0 ; j < nums.length +1 ; j++){
-            Arrays.fill(dp[i][j] , -1 ) ; 
-           }
+        int n = nums.length ;
+
+        // as we can see here there are 3 changing state i , prev and k 
+        int dp [][][] = new int[n][n+1][k+1];
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n + 1 ;j++){
+                Arrays.fill(dp[i][j] , -1 ) ; 
+            }
         }
-        return f(0 , -1 ,  nums , k , dp  ) ; 
+
+
+        
+        // here we take the index , prev to keep track of the previous , and nums , and k 
+        return f(0 , -1 , nums , k , dp ) ; 
     }
 }
