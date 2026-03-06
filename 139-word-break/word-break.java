f9cc7323ef48b4a444bfec6c970ee5c25ff72c96
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-03-06 08:11:54 +0530

    Time: 7 ms (53.97%) | Memory: 45.9 MB (58.31%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,32 +1,27 @@
 class Solution {
     public boolean f(int i , Set<String> set , String s , Boolean dp[]){
-        int n = s.length();
-        if( i >= n ) return true ; 
+        if(i >= s.length()) return true; 
 
-        if(dp[i] != null) return dp[i];
+        if(dp[i] != null ) return dp[i] ; 
 
-        for(int j = i ; j < n ; j++){
-            String curr = s.substring(i , j+1 );
-            if(set.contains(curr) ){
-                if(f( j + 1  ,set, s , dp))
-                return dp[i]=  true;
+        for(int j = i ; j < s.length() ; j++){
+            String curr = s.substring(i , j + 1 );
+            if(set.contains(curr) && f(j+1 , set , s , dp ) ){
+                
+                return dp[i] = true;
             }
         }
-        return dp[i] = false;
+        return dp[i] = false; 
     }
-    public boolean wordBreak(String s, List<String> list) {
-        Set<String> set = new HashSet<>();
-        for(int i =0 ; i < list.size() ; i++){
-            set.add(list.get(i));
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet <>();
+
+        for(int i = 0; i< wordDict.size() ; i++){
+            set.add(wordDict.get(i));
         }
-        int n = s.length();
 
-        Boolean dp[] = new Boolean [n];
+        Boolean dp[] = new Boolean [s.length()];
 
-        
-
-        return f(0 , set , s ,dp) ; 
-
-
+        return f(0 , set , s , dp);
     }
 }
