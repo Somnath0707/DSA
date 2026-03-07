309053fe46dc586ea097734481826dd34232a073
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-03-07 08:09:25 +0530

    Time: 146 ms (7.02%) | Memory: 66.8 MB (5.50%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
@@ -1,24 +1,32 @@
 class Solution {
-    public int f(int i , int prev , int [][] nums , int dp[][]){
-        if( i == nums.length ) return 0 ; 
 
-        if(dp[i][prev+1] != -1001) return dp[i][prev+1];
+    public int f(int i , int prev , int[][] nums , int[][] dp){
+        if(i == nums.length ) return 0 ;
 
-        int skip = f(i +1 , prev , nums , dp);
-        int take = 0 ;
-        if(prev == -1 || (nums[i][0]>nums[prev][1])){
-            take = 1 + f(i+1 , i , nums , dp);
+        if(dp[i][prev+1] != -1 ) return dp[i][prev+1];
+
+        int skip = f(i+1 , prev , nums , dp);
+
+        int take = 0; 
+
+        if(prev == -1 || (nums[i][0] > nums[prev][1])){
+            take = 1 + f(i +1 , i , nums , dp); 
         }
 
-        return dp[i][prev+1] = Math.max(take , skip );
+        return dp[i][prev+1] =  Math.max(take , skip);
+
+
     }
     public int findLongestChain(int[][] pairs) {
-        Arrays.sort(pairs, (a, b ) -> a[0] - b[0]);
-        int dp[][] = new int [pairs.length][pairs.length+1 ];
-        for(int i =0 ; i < pairs.length ; i++){
-            Arrays.fill(dp[i] , -1001);
+        int n = pairs.length;
+        Arrays.sort(pairs, (a , b) -> a[0] -b[0]);
+        int dp[][] = new int [n][n+1];
+
+        for(int i = 0; i < n ; i++){
+            Arrays.fill(dp[i] , -1);
         }
 
         return f(0 , -1 , pairs , dp);
+
     }
 }
