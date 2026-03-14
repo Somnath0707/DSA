3d2896e8b2c6f501b25e28660db91537dd29329b
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-03-14 12:50:33 +0530

    Time: 16 ms (59.06%) | Memory: 47.5 MB (85.06%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,65 +1,59 @@
 class Triple{
     int distance;
-    int row ; 
-    int col ;
+    int row;
+    int col;
 
-    Triple (int d , int r , int c ) {
-        this.distance = d ; 
-        this.row = r; 
-        this.col = c;
+    Triple(int d,int r,int c){
+        distance=d;
+        row=r;
+        col=c;
     }
 }
-
 
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
 
-        if(grid[0][0] == 1 ) return -1 ;
-        // check if src and dst are same 
+        int n = grid.length;
+
+        if(grid[0][0]==1 || grid[n-1][n-1]==1) return -1;
+
         Queue<Triple> q = new LinkedList<>();
 
-        int n = grid.length ; 
-        int m = grid[0].length;
+        int[][] dist = new int[n][n];
 
-        int[][] distance = new int[n][m];
+        for(int i=0;i<n;i++)
+            Arrays.fill(dist[i],Integer.MAX_VALUE);
 
-        for(int i = 0; i< n ; i++){
-            Arrays.fill(distance[i] , Integer.MAX_VALUE);
-        }
+        dist[0][0]=1;
 
-        distance[0][0] = 0 ; 
+        q.add(new Triple(1,0,0));
 
-        q.add(new Triple ( 0 , 0 , 0 ));
-        int[] dr = {-1,-1,-1,0,0,1,1,1};
-        int[] dc = {-1,0,1,-1,1,-1,0,1};
-
+        int[] dr={-1,-1,-1,0,0,1,1,1};
+        int[] dc={-1,0,1,-1,1,-1,0,1};
 
         while(!q.isEmpty()){
-            Triple curr = q.poll();
-            int dis = curr.distance; 
-            int r = curr.row; 
-            int c = curr.col;
 
-            for(int i = 0; i< 8 ; i++){
-                int newRow = r + dr[i];
-                int newCol = c + dc[i];
+            Triple curr=q.poll();
 
+            int d=curr.distance;
+            int r=curr.row;
+            int c=curr.col;
 
-                if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && grid[newRow][newCol] == 0 && dis + 1 < distance[newRow][newCol]){
-                    distance[newRow][newCol] = 1 + dis;
-                    q.add(new Triple(distance[newRow][newCol] , newRow , newCol));
+            if(r==n-1 && c==n-1) return d;
+
+            for(int i=0;i<8;i++){
+
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+
+                if(nr>=0 && nc>=0 && nr<n && nc<n && grid[nr][nc]==0 && d+1<dist[nr][nc]){
+
+                    dist[nr][nc]=d+1;
+                    q.add(new Triple(d+1,nr,nc));
                 }
             }
         }
 
-        for(int i = 0 ; i < distance.length ; i++){
-            for(int j = 0; j < distance[0].length ; j++){
-                if(distance[i][j] == Integer.MAX_VALUE){
-                    distance[i][j] = -1 ; 
-                }
-            }
-        }
-        if(distance[m-1][n-1] == -1 ) return -1 ; 
-        return distance[m-1][n-1] + 1 ;
+        return -1;
     }
 }
