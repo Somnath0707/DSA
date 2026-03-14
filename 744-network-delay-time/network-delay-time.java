6ab7b12ecb6251d51ded263654fb53550cc999a9
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-03-14 11:56:27 +0530

    Time: 10 ms (86.25%) | Memory: 49.5 MB (43.88%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,54 +1,48 @@
 class Pair{
-    int distance ;
+    int distance ; 
     int node ; 
-
+    
     Pair(int d , int n ){
         this.distance = d ; 
-        this.node = n; 
+        this.node = n ;
     }
 }
 
-
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        
-        // create a new list 
+        // Make a adj list 
         List<List<Pair>> adj = new ArrayList<>();
 
-        // make a outine of this
-        for(int i = 0; i<= n ; i++){
+        for(int i = 0; i<= n ; i ++){
             adj.add(new ArrayList<>());
         }
+        // put the elemnt in the adj list 
 
+        for(int i = 0 ; i< times.length ; i ++){
+            int u = times[i][0]; // this is the curr node 
+            int v = times[i][1];// the connection to the other node i.e which node is conntectd
+            int wt = times[i][2]; // distance from curr to next node
 
-        // add the connection along with the weight 
-        for(int i = 0; i < times.length ; i++){
-            int u = times[i][0];
-            int v = times[i][1];
-            int wt = times[i][2];
-
-            adj.get(u).add(new Pair(wt , v));
-
+            adj.get(u).add(new Pair(wt , v));// one direction only so 
         }
 
+        int distance[] = new int[n+1]; // 1 indexed array
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>((x, y ) -> x.distance - y.distance);
-
-        int distance[] = new int[n+1];
         Arrays.fill(distance , Integer.MAX_VALUE);
-        if(k > times.length) return -1; 
+
         distance[k] = 0 ; 
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>((x,y) -> x.distance - y.distance);
 
         pq.add(new Pair(0 , k));
 
         while(!pq.isEmpty()){
             Pair curr = pq.poll();
-
-            int currDis = curr.distance; 
+            int currDis = curr.distance ; 
             int currNode = curr.node;
 
             for(Pair it : adj.get(currNode)){
-                int nextDis = it.distance;
+                int nextDis = it.distance ; 
                 int nextNode = it.node;
 
                 if(distance[nextNode] > currDis + nextDis){
@@ -58,11 +52,13 @@ class Solution {
                 }
             }
         }
-        int ans = -1 ; 
-        for(int i = 1 ; i < n+1 ; i++){
-            if(distance[i] == Integer.MAX_VALUE) return -1 ; 
+        int ans = 0 ; 
+        for(int i = 1; i < distance.length; i++){
+            if(distance[i] == Integer.MAX_VALUE){
+                return -1; 
+            }
             ans = Math.max(ans , distance[i]);
         }
-        return ans;
+        return ans ; 
     }
 }
