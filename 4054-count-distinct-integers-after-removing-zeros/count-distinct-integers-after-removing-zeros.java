617038243ad7bb11247af99e93f23d7909b4b94a
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-03-20 11:36:20 +0530

    Time: 5 ms (13.48%) | Memory: 44.9 MB (7.09%) - LeetSync

--- 4054-count-distinct-integers-after-removing-zeros/count-distinct-integers-after-removing-zeros.java
+++ 4054-count-distinct-integers-after-removing-zeros/count-distinct-integers-after-removing-zeros.java
@@ -1,41 +1,41 @@
 class Solution {
-    long f(int i, int tight, String s , long dp[][]) {
-        if (i == s.length()) return 1;
-
-        int limit = (tight == 1) ? s.charAt(i) - '0' : 9;
-        long count = 0;
-        if(dp[i][tight] != -1 ) return dp[i][tight];
-
-        for (int d = 1; d <= 9; d++) {
-            if (d > limit) break;
-
-            int nextTight = (tight == 1 && d == limit) ? 1 : 0;
-            count += f(i + 1, nextTight, s , dp);
-        }
-
-        return dp[i][tight] = count;
-    }
-
-    public long countDistinct(long n) {
-        String s = String.valueOf(n);
-        long dp[][] = new long[20][2];
-        for(int i = 0 ; i < 20 ; i++){
-            Arrays.fill(dp[i] , -1 );
-        }
-        long len = s.length();
-        
-        long ans = 0;
-
-        // count numbers with smaller length
-        for (int l = 1; l < len; l++) {
-            ans += Math.pow(9, l);
-        }
-
-        // count same length
-        ans += f(0, 1, s , dp);
-
-        return ans;
-    }
-
     
+    public long f(String s , int i , int tight , int leadZero , long dp[][][]){
+        int n = s.length ();
+
+        if(i >= n ){
+            if(leadZero == 1){
+                return 0 ;
+            }
+            return 1 ; 
+        }
+        if(dp[i][tight][leadZero] != -1) return dp[i][tight][leadZero] ;
+
+
+        int limit = (tight == 1 ) ? s.charAt(i) -'0' : 9 ;
+        long ans = 0 ; 
+
+        for(int dig = 0  ; dig <= limit ; dig++){
+            int nextTight = (tight == 1 && dig == limit) ? 1 : 0 ; 
+            if(leadZero == 1 && dig == 0){
+                ans += f(s , i + 1 , nextTight , 1 , dp);
+            }
+            else{
+                if(dig == 0 ) continue;
+                ans += f(s , i+1 , nextTight , 0 , dp );
+            }
+        }
+        return dp[i][tight][leadZero] = ans ;
+    }
+    public long countDistinct(long n) {
+        
+        String s = String.valueOf(n);
+        long dp[][][] = new long[20][2][2];
+        for(int i =0 ;i < 20 ; i ++){
+            for(int j = 0 ;  j < 2 ; j++){
+                Arrays.fill(dp[i][j] , -1);
+            }
+        }
+        return f(s , 0 , 1 , 1 , dp);
+    }
 }
