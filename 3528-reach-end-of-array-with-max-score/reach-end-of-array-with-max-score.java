3ca60794717576aa980b78dc07cdbca00a06905e
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-03-21 20:53:12 +0530

    Time: 9 ms (82.56%) | Memory: 164.4 MB (37.21%) - LeetSync

---
+++ 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long findMaximumScore(List<Integer> nums) {
+        long totalScore = 0;
+        long currentMax = 0;
+        
+        // We iterate through all elements except the last one
+        for (int i = 0; i < nums.size() - 1; i++) {
+            // Update the best multiplier we've seen so far
+            currentMax = Math.max(currentMax, (long)nums.get(i));
+            
+            // Add the current best multiplier to the score for this "step"
+            totalScore += currentMax;
+        }
+        
+        return totalScore;
+    }
+}
