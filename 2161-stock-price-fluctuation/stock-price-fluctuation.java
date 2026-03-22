7c96a99cdf7e87416533e917a0d065fe03cd133e
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-03-22 19:28:40 +0530

    Time: 155 ms (10.20%) | Memory: 129.5 MB (81.58%) - LeetSync

--- 2161-stock-price-fluctuation/stock-price-fluctuation.java
+++ 2161-stock-price-fluctuation/stock-price-fluctuation.java
@@ -1,27 +1,26 @@
 class StockPrice {
-    TreeMap<Integer, Integer> map;   // price -> freq
-    TreeMap<Integer, Integer> time;  // timestamp -> price
-
+    TreeMap<Integer, Integer> map;
+    TreeMap<Integer, Integer> time;
+    int removeCurr;
     public StockPrice() {
         map = new TreeMap<>();
         time = new TreeMap<>();
     }
     
     public void update(int timestamp, int price) {
+        if(time.containsKey(timestamp)){
+            removeCurr = time.get(timestamp);
 
-        // ✅ only remove if timestamp already exists
-        if (time.containsKey(timestamp)) {
-            int oldPrice = time.get(timestamp);
-
-            map.put(oldPrice, map.get(oldPrice) - 1);
-            if (map.get(oldPrice) == 0) {
-                map.remove(oldPrice);
+        
+            map.put(removeCurr , map.get(removeCurr)-1 );
+            if(map.get(removeCurr) == 0) {
+                map.remove(removeCurr);
             }
         }
 
-        // update new price
-        time.put(timestamp, price);
-        map.put(price, map.getOrDefault(price, 0) + 1);
+        time.put(timestamp , price);
+        map.put(price , map.getOrDefault(price , 0 ) + 1 ) ; 
+        
     }
     
     public int current() {
@@ -29,10 +28,19 @@ class StockPrice {
     }
     
     public int maximum() {
-        return map.lastKey();
+       return map.lastKey();
     }
     
     public int minimum() {
         return map.firstKey();
     }
 }
+
+/**
+ * Your StockPrice object will be instantiated and called as such:
+ * StockPrice obj = new StockPrice();
+ * obj.update(timestamp,price);
+ * int param_2 = obj.current();
+ * int param_3 = obj.maximum();
+ * int param_4 = obj.minimum();
+ */
