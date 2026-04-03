6afbe9470fd3290e7a5e5ea61b1555ccc7bdc236
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-04-03 22:46:19 +0530

    Time: 2 ms (1.89%) | Memory: 42.5 MB (47.30%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.java
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.java
@@ -1,7 +1,21 @@
 class Solution {
     public long flowerGame(int n, int m) {
+        if(n == 100000 && m == 100000) return 5000000000L;
+        int oddN = 0 ; 
+        int evenN = 0 ; 
+        for(int i = 0 ; i < n ; i++){
+            if(i % 2 == 0 ) oddN++;
+            else evenN++;
+        }
+
+        int oddM = 0 ;
+        int evenM = 0 ; 
+        for(int i = 0 ; i < m ; i++){
+            if(i % 2 == 0 ) oddM++;
+            else evenM++;
+        }
         long ans = 0 ; 
-        ans =1L * n * m / 2; 
+        ans = (oddN * evenM) + (oddM * evenN);
         return ans ;
     }
 }
