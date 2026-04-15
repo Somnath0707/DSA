541e7cd64f8d370215dbd62707eb02dc8d18b043
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-04-15 08:11:14 +0530

    Time: 70 ms (29.17%) | Memory: 56.7 MB (62.50%) - LeetSync

--- 4071-word-squares-ii/word-squares-ii.java
+++ 4071-word-squares-ii/word-squares-ii.java
@@ -1,38 +1,32 @@
-import java.util.*;
-
 class Solution {
     public List<List<String>> wordSquares(String[] words) {
         List<List<String>> ans = new ArrayList<>();
-        int n = words.length;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j == i) continue;
-
-                for (int k = 0; k < n; k++) {
-                    if (k == i || k == j) continue;
-
-                    for (int l = 0; l < n; l++) {
-                        if (l == i || l == j || l == k) continue;
+        for(int i = 0 ; i < words.length ; i++){
+            for(int j = 0 ; j < words.length ; j++){
+                if(i == j ) continue; 
+                for(int k = 0 ; k < words.length ; k++){
+                    if(i == k || j == k ) continue;
+                    for(int l = 0 ; l < words.length ; l++){
+                        if(l == j || l == k || i == l) continue;
 
                         String top = words[i];
-                        String left = words[j];
+                        String left= words[j];
                         String right = words[k];
                         String bottom = words[l];
 
-                        if (top.charAt(0) == left.charAt(0) &&
-                            top.charAt(3) == right.charAt(0) &&
-                            bottom.charAt(0) == left.charAt(3) &&
-                            bottom.charAt(3) == right.charAt(3)) {
 
-                            ans.add(Arrays.asList(top, left, right, bottom));
+                        if(top.charAt(0) == left.charAt(0) &&
+                           top.charAt(3) == right.charAt(0) &&
+                           bottom.charAt(0) == left.charAt(3) &&
+                           bottom.charAt(3) == right.charAt(3)){
+                            ans.add(Arrays.asList(top , left , right , bottom));
                         }
                     }
                 }
             }
         }
-
-        ans.sort((a, b) -> a.toString().compareTo(b.toString()));
-        return ans;
+        ans.sort((a,b) -> a.toString().compareTo(b.toString()));
+        return ans ;
     }
 }
