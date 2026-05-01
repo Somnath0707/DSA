57c48feb5c68ed367e98bf01e698e90be4c8c3a0
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-05-01 08:19:01 +0530

    Time: 28 ms (63.10%) | Memory: 230.7 MB (52.38%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -1,55 +1,45 @@
 class Solution {
 
-    public boolean check(int k , int nums[] , int queries[][]){
+    public boolean check(int k , int[] nums , int queries[][]){
         int n = nums.length ; 
 
-        int diffArr[] = new int[nums.length];
+        int [] diffArr = new int[n];
 
+        for(int i = 0 ; i < k ;i++){
+            int left = queries[i][0];
+            int right = queries[i][1]; 
+            int val = queries[i][2];
 
-        // Check for the first few queries 
-        for(int i = 0 ; i < k ; i++){
-            int curr[] = queries[i];
-            int left = curr[0];
-            int right = curr[1];
-            int val = curr[2];
-
-            diffArr[left] += val;
+            diffArr[left] += val ;
             if(right + 1 < n ){
-                diffArr[right+1] -= val;
+                diffArr[right + 1] -= val ; 
             }
         }
-
-        int curr = 0; 
-        for(int i =0 ; i < n ; i++){
-            curr += diffArr[i];
-
-            if(curr < nums[i]){
-                return false ; 
-            }
+        int curr = 0 ; 
+        for(int i = 0 ; i < n ; i++){
+            curr += diffArr[i]; 
+            if(curr < nums[i]) return false; 
         }
-        return true; 
-        
+        return true ; 
 
-    }
+    }   
+
     public int minZeroArray(int[] nums, int[][] queries) {
-
         int left = 0 ; 
         int right = queries.length ; 
 
-        int ans = - 1; 
+        int ans = -1 ; 
 
         while(left <= right ){
-            int mid = left + (right - left) / 2; 
+            int mid = left + (right - left ) / 2 ; 
 
-            if(check(mid , nums , queries)){
+            if(check( mid , nums , queries)){
                 ans = mid ; 
                 right = mid -1 ; 
             }else {
                 left = mid + 1 ; 
             }
         }
-
-        return ans; 
-
+        return ans;
     }
 }
