6ee8ea8c55b9ec84b135619108e5284f80983564
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-05-06 08:15:15 +0530

    Time: 9 ms (35.58%) | Memory: 124.5 MB (91.11%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.java
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -1,40 +1,39 @@
 class Solution {
-    public char[][] rotateTheBox(char[][] nums) {
-        int n = nums.length ; 
-        int m = nums[0].length ;
-        for(int i = 0 ; i < n ; i++){
-            int start = -1 ; 
-            for(int j = 0 ; j < m ; j++){
-                char ch = nums[i][j];
-                if(ch == '*'){
-                    start = -1;
-                }
-                if(ch == '.' && start != -1 ){
-                    nums[i][start] = '.';
-                    nums[i][j] = '#';
-                    start++;
-                }
-                if(ch == '#' && start == -1 ){
-                    start = j;
-                }
-            }
-        }
-
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ;j++){
-                System.out.print(nums[i][j]);
-            }
-            System.out.println();
-        }
-
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int n = boxGrid.length; 
+        int m = boxGrid[0].length;
         char ans[][] = new char[m][n];
+
         int ind = 0 ; 
-        for(int i = n-1 ; i >= 0 ; i-- ){
+        for(int i = n-1 ; i >= 0 ; i--){
+            int startHash = -1 ; 
+            char prev = '@';
+            int jInd = 0 ; 
             for(int j = 0 ; j < m ; j++){
-                ans[j][ind] = nums[i][j];
+                char ch = boxGrid[i][j];
+                if(ch == '#' && startHash == -1 ){
+                    startHash = j;
+                    prev = '#';
+                    ans[j][ind] = '#';
+                }
+                if(ch == '*'){
+                    startHash = -1 ; 
+                    prev = ch;
+                    ans[j][ind] = ch;
+                }
+                if(ch =='.' && prev == '#'){
+                    ans[startHash][ind] = '.';
+                    ans[j][ind] = '#';
+                    startHash++;
+                    prev = '#';
+                }
+                else{
+                    ans[j][ind] = ch;
+                    prev = ch; 
+                }
             }
             ind++;
         }
-    return ans;
+        return ans ; 
     }
 }
