b2c93efd12b5ba205ee0a5d5809abce30dc622cc
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-05-07 07:08:57 +0530

    Time: 30 ms (90.76%) | Memory: 70.8 MB (69.47%) - LeetSync

--- 4257-sum-of-sortable-integers/sum-of-sortable-integers.java
+++ 4257-sum-of-sortable-integers/sum-of-sortable-integers.java
@@ -1,66 +1,48 @@
 class Solution {
-    public boolean checkCond(int k , int [] nums){
-        int n = nums.length ;
+    public boolean check(int k , int[] nums){
+        int n = nums.length; 
         int left = 0 ; 
-        int right = 0; 
         int prevMax = Integer.MIN_VALUE;
-        int prevMin = Integer.MAX_VALUE;
-        
-        while( right < n ){
-
-            int max =nums[left];
-            int min = nums[left];
+        while(left < n ){
             int dropped = 0 ; 
+            int min = nums[left];
+            int max = nums[left];
             for(int i = left+1 ; i < left + k ; i++){
-                if (nums[i] < nums[(i - 1) % n]){
-                    dropped++; 
-                    if (dropped > 1) {
-                        return false; 
+                if(nums[i] < nums[(i-1)]){
+                    dropped++;
+                    if(dropped > 1 ){
+                        return false;
                     }
                 }
-
-                max = Math.max(max , nums[i]);
-                min = Math.min(min , nums[i]);
+                max = Math.max(nums[i] , max);
+                min = Math.min(nums[i] , min);
             }
-            if (dropped == 1 && nums[left + k - 1] > nums[left]) {
-                return false;
+            // If the last element is greater than the first element in the roatated array it cant be sorted 
+            if(dropped == 1 && nums[left + k - 1] > nums[left] ){
+                return false; 
             }
             if(min < prevMax){
-                return false; 
-            }else{
-                prevMin = min;
-                prevMax = max;
-                left += k;
-                right += k;
+                return false;
             }
-            // Check for the drop in the size k of the array 
+            left += k;
+            prevMax = max;
 
         }
-        return true; 
+        return true;
     }
     public int sortableIntegers(int[] nums) {
-        // the the k size window check 
-        int n = nums.length;
-
-        // 7 8 10 1 2 3
-        int sum = 0 ; 
-
-
+        int n = nums.length ; 
+        int count = 0 ; 
         for(int i = 1 ; i <= n ; i++){
-            if(n % i == 0 ){
-                if(checkCond(i , nums)){
-                    System.out.println(" sum till now : " + sum);
-                    sum += i ;
-                    System.out.println(sum);
+
+            if(n % i == 0){
+                if(check(i , nums)){
+                    count += i;
                 }
-            }
-            // Divisor of n 
-            else{
+            }else{
                 continue;
             }
         }
-        return sum ; 
+        return count ; 
     }
 }
-
-// 12,18,14 -> 12 
