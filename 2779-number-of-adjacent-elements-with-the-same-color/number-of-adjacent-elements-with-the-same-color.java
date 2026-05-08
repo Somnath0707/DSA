faa20c86f2c0aad208e8ac7d81bdf7b993fc95ba
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-05-08 21:32:18 +0530

    Time: 5 ms (67.67%) | Memory: 208.3 MB (16.96%) - LeetSync

--- 2779-number-of-adjacent-elements-with-the-same-color/number-of-adjacent-elements-with-the-same-color.java
+++ 2779-number-of-adjacent-elements-with-the-same-color/number-of-adjacent-elements-with-the-same-color.java
@@ -1,55 +1,29 @@
 class Solution {
-    public int[] colorTheArray(int n, int[][] nums) {
-        int len = nums.length;
-        int ans[] = new int[len];
-        int temp[] = new int[n]; // Store colors
-        int count = 0; 
-        // map stores: Index -> Number of neighbors it is currently paired with
-        HashMap<Integer, Integer> map = new HashMap<>();
+    public int[] colorTheArray(int n, int[][] queries) {
+        int[] color = new int[n];
+        int[] ans = new int[queries.length];
+        int count = 0;
 
-        for(int i = 0; i < len; i++){
-            int ind = nums[i][0];
-            int newColor = nums[i][1];
+        for (int i = 0; i < queries.length; i++) {
+            int idx = queries[i][0];
+            int c = queries[i][1];
 
-            // 1. UNDO logic: If index was already colored, remove its current pair contributions
-            if (temp[ind] != 0 && temp[ind] != newColor) {
-                // Check Left
-                if (ind > 0 && temp[ind-1] == temp[ind]) {
-                    count--;
-                    // Update map for both neighbors involved in the broken pair
-                    map.put(ind, map.get(ind) - 1);
-                    map.put(ind-1, map.get(ind-1) - 1);
-                }
-                // Check Right
-                if (ind < n - 1 && temp[ind+1] == temp[ind]) {
-                    count--;
-                    map.put(ind, map.get(ind) - 1);
-                    map.put(ind+1, map.get(ind+1) - 1);
-                }
+            // remove old contribution
+            if (color[idx] != 0) {
+                if (idx > 0 && color[idx] == color[idx - 1]) count--;
+                if (idx < n - 1 && color[idx] == color[idx + 1]) count--;
             }
 
-            // 2. APPLY the new color (only if it's actually a change or first time)
-            if (temp[ind] != newColor) {
-                temp[ind] = newColor;
+            // paint new color
+            color[idx] = c;
 
-                // 3. REDO logic: Check if the new color creates new pairs
-                // Check Left
-                if (ind > 0 && temp[ind-1] == temp[ind]) {
-                    count++;
-                    map.put(ind, map.getOrDefault(ind, 0) + 1);
-                    map.put(ind-1, map.getOrDefault(ind-1, 0) + 1);
-                }
-                // Check Right
-                if (ind < n - 1 && temp[ind+1] == temp[ind]) {
-                    count++;
-                    map.put(ind, map.getOrDefault(ind, 0) + 1);
-                    map.put(ind+1, map.getOrDefault(ind+1, 0) + 1);
-                }
-            }
+            // add new contribution
+            if (idx > 0 && color[idx] == color[idx - 1]) count++;
+            if (idx < n - 1 && color[idx] == color[idx + 1]) count++;
 
             ans[i] = count;
         }
-       
+
         return ans;
     }
 }
