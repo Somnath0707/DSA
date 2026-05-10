d18c7b41215d69dade991b134f6f01aedc5f77e7
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-05-11 00:23:03 +0530

    Time: 840 ms (5.36%) | Memory: 280.9 MB (8.33%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.java
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.java
@@ -1,33 +1,29 @@
 class Solution {
-    Integer dp[][] ; 
-    public int f(int i , int j , int[] nums , int k ){
-        int n = nums.length ; 
+    Map<String , Integer> dp ; 
+    public int f(int i , int j , int nums[] , int k ) {
+        int n = nums.length; 
+        String key = i + "#" + j ; 
         if(i == n-1) return 0 ; 
-        if(j >= n) return -10000000;
-        
-        
-        
-        if(dp[i][j] != null ) return dp[i][j]; 
+        if( j >= n ) return -100000000;
+        if(dp.containsKey(key)) return dp.get(key);
+        int take = -100000000;
+        int skip = -1 ; 
 
-        int take = -10000000 ; 
-        int skip = -1 ;
-
-        if(i < j && nums[j] - nums[i] <= k && nums[j] - nums[i] >= -k){
-            take = 1 + f(j , j+1 , nums , k );
+        if(i < j && nums[j] - nums[i] <= k && nums[j] - nums[i] >= -k ){
+            take = 1 + f(j , j+1 , nums , k);
         }
 
         skip = f(i , j+1 , nums , k );
 
-        return dp[i][j] =  Math.max(take , skip);
-        
-
-
+        int ans = Math.max(skip , take );
+        dp.put(key , ans);
+        return ans;
     }
     public int maximumJumps(int[] nums, int target) {
-        int n = nums.length ;
-        dp = new Integer[n][n];
-        int ans = f(0 , 1 , nums , target);
-        if(ans < 0 ) return -1 ; 
-        return ans; 
+        dp = new HashMap<>();
+        int ans = f(0 , 0 , nums , target);
+        if(ans < 0 ) return -1; 
+
+        return ans ; 
     }
 }
