8c80bd4813da3c71afc23d80132849b98ada096c
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-05-11 19:19:04 +0530

    Time: 85 ms (77.24%) | Memory: 46.7 MB (82.68%) - LeetSync

--- 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
@@ -1,53 +1,42 @@
 class Solution {
-    public int bfs(int j , int [] curr , int bombs[][]){
-        int n = bombs.length;
+    public int bfs(int j , int [] currBomb , int[][] bombs){
         Queue<int[]> q = new LinkedList<>();
+        int n = bombs.length;
 
-        q.offer(curr);
+        q.offer(currBomb);
         boolean vis[] = new boolean[n];
-        vis[j] = true;
-        int count = 1 ; 
+        vis[j] = true; 
+        int count = 1; 
         while(!q.isEmpty()){
-            int[] temp = q.poll();
-            int x1 = temp[0]; 
-            int y1 = temp[1];
-            int r1 = temp[2];
+            int curr[] = q.poll();
+            int x1 = curr[0];
+            int y1 = curr[1];
+            int r1 = curr[2];
             for(int i = 0 ; i < n ; i++){
-                // check if not vis and also within th explosion range of the curr
                 int x2 = bombs[i][0];
                 int y2 = bombs[i][1];
-                int r2 = bombs[i][2];
-
                 if(!vis[i]){
-                    long X = (long)x1 - x2;
-                    long Y =(long)y1 - y2;
+                    long X = x1-x2;
+                    long Y = y1 - y2;
 
-                    long check = 1L * X*X + Y*Y;
-                    if(check <= 1L * r1*r1){
+                    long check = X*X + Y*Y;
+                    if(check <= 1L * r1 * r1){
+                        count++;
+                        vis[i] = true; 
                         q.offer(bombs[i]);
-                        vis[i] = true;
-                        count++; 
                     }
-                } 
+                }
             }
         }
-        return count; 
+        return count;
     }
     public int maximumDetonation(int[][] bombs) {
-        // take the bomb an detonate it 
-        // we get the new range for acircle 
-        // check if there are pair that can detonate within this range try to do it if so detnoate it and try to do like bfs denote the other too and do this for each bomb and get the max number of bomb we can detonate 
-
-        // detonate one then do bfs all the detonatebal add them to queue then detonate that one and do bfs again but keep the vis track 
-        // do it again till the queueis empty and keep the track all the poll ones 
-        
-        // use for loop to go through all the bombs 
-        /// Complexity would be 0n2
+        int n = bombs.length ;
         int ans = 0 ; 
-        for(int i = 0 ; i < bombs.length ; i++){
-            ans = Math.max(ans , bfs(i , bombs[i] , bombs));
+        for(int i = 0 ; i < n ; i++){
+            ans = Math.max(ans ,bfs(i , bombs[i] , bombs));
         }
 
-        return ans;
+        return ans ; 
     }
 }
