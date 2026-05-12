4dd6c70b99018b869dc8b7e91dc6c7e6c628c0f7
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-05-12 08:30:48 +0530

    Time: 92 ms (5.77%) | Memory: 126.6 MB (5.77%) - LeetSync

--- 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.java
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.java
@@ -1,50 +1,32 @@
 class Solution {
-    public boolean isAns(int n , int[][] nums){
-        for(int i = 0 ; i < nums.length; i++){
-            int min = nums[i][1];
-            int act = nums[i][0];
-            int diff = nums[i][2];
-
-            if(n >= min){
-                n -= act;
-                continue;
+    public int minimumEffort(int[][] tasks) {
+        int start = 0;
+        int end = 0;
+        for(int[] task: tasks){
+            start += task[0];
+            end += task[1];
+        }
+        int res = Integer.MAX_VALUE;
+        Arrays.sort(tasks, (a, b) -> { return (b[1] - b[0]) - (a[1] - a[0]);});
+        System.out.println(Arrays.deepToString(tasks));
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(isValid(tasks, mid)){
+                res = Math.min(res,mid);
+                end = mid-1;
+            }else{
+                start = mid+1;
             }
-            if(n < min){
+        }
+        return res;
+    }
+    boolean isValid(int[][] tasks, int num){
+        for(int[] task: tasks){
+            if(num<task[1]){
                 return false;
             }
+            num -= task[0];
         }
-        return true; 
-    }
-    public int minimumEffort(int[][] tasks) {
-        int n = tasks.length;
-        int temp[][] = new int[n][3];
-        for(int i = 0 ; i < n ; i++){
-            temp[i][0] = tasks[i][0];
-            temp[i][1] = tasks[i][1];
-            int diff = tasks[i][1] - tasks[i][0];
-            temp[i][2] = diff;
-        }
-
-        Arrays.sort(temp , (a,b) ->{
-            if(a[2] != b[2]) return b[2] - a[2];
-            else if(a[1] != b[1]) return b[1] - a[1];
-            else return b[0] - a[0];
-        });
-
-        int left = 1 ; 
-        int right = 1000000000;
-        int ans = 0  ;
-
-        while(left <= right){
-            int mid = left + (right - left) / 2; 
-
-            if(isAns(mid , temp)){
-                ans = mid ; 
-                right = mid - 1 ; 
-            }else {
-                left = mid + 1 ;
-            }
-        }
-        return ans; 
+        return true;
     }
 }
