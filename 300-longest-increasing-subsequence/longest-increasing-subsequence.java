ba237ed395139c4d440a35d48da2c55ef6ea8510
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-05-14 12:02:56 +0530

    Time: 334 ms (14.86%) | Memory: 272.8 MB (6.80%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,29 +1,27 @@
 class Solution {
 
-    public int lengthOfLIS(int[] nums) {
+    Integer dp[][] ; 
+    public int f(int i , int j , int [] nums){
+        if(i >= nums.length) return 0 ; 
+        if(j >= nums.length ) return 0 ; 
+        if(dp[i][j] != null) return dp[i][j];
 
-        List<Integer> tails = new ArrayList<>();
-
-        for(int num : nums){
-
-            int l = 0, r = tails.size();
-
-            while(l < r){
-
-                int mid = (l+r)/2;
-
-                if(tails.get(mid) < num)
-                    l = mid + 1;
-                else
-                    r = mid;
-            }
-
-            if(l == tails.size())
-                tails.add(num);
-            else
-                tails.set(l, num);
+        int take = 0 ; 
+        if(nums[j] > nums[i]){
+            take = 1 + f(j , j+1 , nums);
         }
 
-        return tails.size();
+        int skip = f(i , j+1 , nums);
+
+        return dp[i][j] =  Math.max(skip , take);
+    }
+    public int lengthOfLIS(int[] nums) {
+        int ans = 0;
+        dp = new Integer[nums.length][nums.length];
+        for(int i = 0 ; i < nums.length; i++){
+            ans = Math.max(ans , 1 + f(i , i , nums));
+        }
+
+        return ans; 
     }
 }
