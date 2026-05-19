a13535cc2d392892928e9e3564ba8bdfa1414076
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-05-19 13:45:36 +0530

    Time: 13 ms (86.32%) | Memory: 47 MB (66.40%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -1,12 +1,12 @@
 class Solution {
     public int minCost(int[][] grid) {
-        // If same side then we do add to front else we add to the back 
-        // We increment the distance only if the side changes else we put just 0 side change we put +1 
-        int n = grid.length;
+        int n = grid.length ;
         int m = grid[0].length;
-        Deque<int[]> dq = new ArrayDeque<>();
-        int dis[][] = new int[n][m];
         
+        Deque<int[]> dq = new ArrayDeque<>();
+
+        // distance array
+        int dis[][] = new int[n][m];
         for(int i = 0 ; i < n ; i++){
             Arrays.fill(dis[i] , Integer.MAX_VALUE);
         }
@@ -14,44 +14,49 @@ class Solution {
         dis[0][0] = 0 ; 
         dq.offerFirst(new int[]{0 , 0});
 
+        // Direction as per question 
         int dir[][] = {
-            {0,1},
-            {0,-1},
-            {1,0},
-            {-1,0}
+            {0 , 1}, // right 
+            {0, -1},// left
+            {1 , 0}, // up 
+            {-1 , 0} // down 
         };
 
         while(!dq.isEmpty()){
             int curr[] = dq.pollFirst();
             int row = curr[0];
-            int col = curr[1]; 
+            int col = curr[1];
 
-            for(int d = 0 ; d < 4 ; d++){
-                int nr = row + dir[d][0];
-                int nc = col + dir[d][1];
+            // All four direction 
+            for(int k = 0 ; k < 4 ; k++){
+                int nr = row + dir[k][0];
+                int nc = col + dir[k][1];
 
-                if(nr >= n || nr < 0 || nc >= m || nc < 0  ){
+                if(nr >= n || nr < 0 || nc >= m || nc < 0 ){
                     continue;
                 }
-                // Same direction 
-                int wt  =  0 ; 
-                if(grid[row][col] == d+1){
-                     wt = 0 ; 
-                }else{
-                    wt = 1 ;
+                int wt = 0 ; 
+                if(grid[row][col] == k + 1 ){
+                    wt = 0 ;
+                }else {
+                    wt = 1; 
                 }
 
-                if(dis[row][col] + wt < dis[nr][nc]){
-                    dis[nr][nc] = dis[row][col] + wt;
-                    if(wt == 0 )
-                    dq.offerFirst(new int[]{nr, nc});
 
-                    if(wt == 1 ){
-                        dq.offerLast(new int[]{nr , nc});
+                if(dis[row][col] + wt  < dis[nr][nc]){
+                    dis[nr][nc] = dis[row][col] + wt;
+
+                    if(wt == 0){
+                        dq.offerFirst(new int[]{nr, nc});
+                    }else{
+                        dq.offerLast(new int[]{nr, nc});
                     }
+
                 }
             }
+
         }
+
         return dis[n-1][m-1];
     }
 }
