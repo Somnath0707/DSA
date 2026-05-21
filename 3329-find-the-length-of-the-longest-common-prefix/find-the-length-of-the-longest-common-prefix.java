b66635dd9a72f5e2eed6cca552d77a8870bc6b14
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-05-21 08:29:39 +0530

    Time: 280 ms (10.86%) | Memory: 83 MB (42.88%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
@@ -11,24 +11,24 @@ class Solution {
             }
         }
 
-        for(int i = 0 ; i < arr2.length ; i++){
-            String str = String.valueOf(arr2[i]);
-            for(int j = 0 ; j < str.length() ; j++){
-                int temp = Integer.parseInt(str.substring(0 , j+1));
-                set2.add(temp);
-            }
-        }
+        // for(int i = 0 ; i < arr2.length ; i++){
+        //     String str = String.valueOf(arr2[i]);
+        //     for(int j = 0 ; j < str.length() ; j++){
+        //         int temp = Integer.parseInt(str.substring(0 , j+1));
+        //         set2.add(temp);
+        //     }
+        // }
         int max = 0 ; 
-        for(int i = 0 ;  i< arr1.length ; i++){
-            String str = String.valueOf(arr1[i]);
-            for(int j = 0 ; j < str.length() ; j++){
-                String s = str.substring(0 , j+1);
-                int temp = Integer.parseInt(s);
-                if(set2.contains(temp)){
-                    max = Math.max(max , s.length());
-                }
-            }
-        }
+        // for(int i = 0 ;  i< arr1.length ; i++){
+        //     String str = String.valueOf(arr1[i]);
+        //     for(int j = 0 ; j < str.length() ; j++){
+        //         String s = str.substring(0 , j+1);
+        //         int temp = Integer.parseInt(s);
+        //         if(set2.contains(temp)){
+        //             max = Math.max(max , s.length());
+        //         }
+        //     }
+        // }
 
 
         for(int i = 0 ; i < arr2.length ; i++){
