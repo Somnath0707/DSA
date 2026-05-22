8005b42809dc98dee258540da29d4758692ca083
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-05-22 07:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (26.61%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,7 +1,9 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i = 0 ; i < nums.length ; i++){
-            if(nums[i] == target) return i ;
+        // check the element 
+        int n = nums.length ; 
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] == target) return i;
         }
         return -1;
     }
