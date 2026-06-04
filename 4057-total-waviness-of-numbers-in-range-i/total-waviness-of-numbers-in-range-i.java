701b5de14c0c377073e5268e7e21cfa17d98ab7a
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-06-04 07:01:01 +0530

    Time: 25 ms (81.41%) | Memory: 46.8 MB (8.18%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
@@ -1,24 +1,20 @@
 class Solution {
     public int totalWaviness(int num1, int num2) {
         int ans = 0 ; 
-        for(int i= num1 ; i <= num2 ; i++){
-            String str = Integer.toString(i);
-            int n = str.length() ; 
-            int wave = 0 ; 
+        for(int i = num1 ; i <= num2 ; i++){
+            String str = String.valueOf(i);
             int peak = 0 ; 
-            for(int j = 1 ; j < n -1 ; j++){
-                int curr = str.charAt(j) - '1';
-                int prev = str.charAt(j-1) - '1';
-                int next = str.charAt(j+1) - '1';
-                if(curr > prev && curr > next){
-                    wave++;
-                }
-                else if(curr < prev && curr < next){
-                    peak ++;
-                }
+            int valley = 0 ; 
+            for(int j = 1 ; j < str.length()-1 ; j++){
+                int curr = str.charAt(j) -'0';
+                int prev = str.charAt(j-1) - '0';
+                int next = str.charAt(j+1) -'0';
+
+                if(curr > prev && curr > next) peak++;
+                else if(curr < prev && curr < next) valley++; 
             }
-            ans += wave + peak;
+            ans += peak + valley;
         }
-        return ans;
+        return ans; 
     }
 }
