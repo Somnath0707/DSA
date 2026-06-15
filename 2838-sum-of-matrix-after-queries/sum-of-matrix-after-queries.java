44c4fd1d7a0893268cf5012f36822cff2654d411
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-06-15 08:35:39 +0530

    Time: 20 ms (13.16%) | Memory: 116.9 MB (15.79%) - LeetSync

---
+++ 2838-sum-of-matrix-after-queries/sum-of-matrix-after-queries.java
@@ -0,0 +1,31 @@
+class Solution {
+    public long matrixSumQueries(int n, int[][] queries) {
+        long sum = 0 ; 
+        int rows = n; 
+        int col = n ; 
+        int len = queries.length; 
+        Set<Integer> seenRow = new HashSet<>(); 
+        Set<Integer> seenCol = new HashSet<>(); 
+        for(int i = len-1 ; i>= 0 ; i--){
+            int curr[] = queries[i];
+            int type = curr[0]; 
+            int indx = curr[1]; 
+            int val = curr[2] ; 
+
+            if(type == 0 ){
+                if(col >= 0 && !seenRow.contains(indx)){
+                    seenRow.add(indx);
+                    sum += val * col; 
+                    rows--;
+                }
+            }else{
+                if(rows >= 0 && !seenCol.contains(indx)){
+                    seenCol.add(indx);
+                    sum+= val * rows;
+                    col--; 
+                }
+            }
+        }
+        return sum ; 
+    }
+}
