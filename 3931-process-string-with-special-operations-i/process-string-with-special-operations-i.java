862903da8439f3bc963857428f4a67c8a33523ac
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-06-16 09:13:07 +0530

    Time: 54 ms (8.09%) | Memory: 57.5 MB (8.82%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    
+    public String processStr(String s) {
+        StringBuilder str = new StringBuilder() ; 
+
+        for(char ch : s.toCharArray()){
+            if(ch == '*'){
+                if(str.length() >=1)
+                    str.deleteCharAt(str.length()-1);
+                System.out.println("THis is the op at * " + str.toString());
+            }
+            else if(ch == '#'){
+                str.append(str);
+                System.out.println("THis is the op at #  " + str);
+            }
+            else if(ch == '%'){
+                str = str.reverse(); 
+                System.out.println("THis is the op at %  " + str);
+            }
+
+            else{
+                str.append(ch);
+                System.out.println("THis is the op at else " + str);
+            }
+        }
+
+        return str.toString();
+    }
+}
