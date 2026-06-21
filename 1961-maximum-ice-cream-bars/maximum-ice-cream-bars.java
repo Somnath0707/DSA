8771e5dfe99072dba59954310ed49961545fc53b
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-06-21 13:40:25 +0530

    Time: 5 ms (91.89%) | Memory: 81.5 MB (99.45%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
@@ -5,22 +5,24 @@ class Solution {
         for(int n : costs){
             arr[n]++;
         } 
-
+        int temp = 0; 
+        int count = 0 ; 
         ArrayList<Integer> list = new ArrayList<>(); 
-        for(int i = 0 ; i < 100001; i++){
-            if(arr[i] != 0) {
+        
+        for(int i = 1 ; i < 100001; i++){
                 while(arr[i] != 0 ){
-                    list.add(i);
-                    arr[i]--;
+                    // System.out.println(temp); 
+                    temp += i ; 
+
+                    // System.out.println("After adding : " + temp); 
+                    if(temp > coins) break;
+                    if(temp <= coins){
+                        // System.out.println(temp + " < " + coins); 
+                        count++; 
+                        arr[i]--;
+                    }
                 }
-            }
-        }
-        int count =0 ; 
-        int temp = 0 ; 
-        for(int i = 0 ; i <list.size() ; i++){
-            temp+= list.get(i);
-            if(temp <= coins) count++; 
-            else break ; 
+                if(temp > coins) break;
         }
 
         return count; 
