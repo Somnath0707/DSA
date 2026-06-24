e91a4dde769ee7bf6b2c86fcb9c0305fc2a70176
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-06-24 10:18:59 +0530

    Time: 16 ms (39.51%) | Memory: 47.8 MB (25.17%) - LeetSync

---
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int minInsertions(String s) {
+        int ans = 0; 
+        int countOpen = 0 ; 
+        int countClose = 0 ; 
+
+        for(int i =0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+
+            // 
+            if(countOpen == 0 && ch == ')'){
+                countOpen++; 
+                countClose++; 
+                ans++; 
+            }
+
+            else if(countOpen >= 1 && countClose == 1 && ch == '('){
+                countOpen--; 
+                countClose = 0 ; 
+                ans++; 
+                countOpen++; 
+            }
+
+            else if(countOpen >= 1 && countClose == 1 && ch == ')'){
+                countOpen--; 
+                countClose = 0 ; 
+            }
+
+            else {
+                if(ch == '(') countOpen++; 
+                if(ch == ')') countClose++ ; 
+            }
+
+        }
+
+        System.out.println(ans);
+        System.out.println(countClose + " " + countOpen);
+
+        if(countOpen != 0){
+            ans += countOpen * 2 - countClose; 
+            return ans; 
+        }
+        if(countClose != 0 ){
+            if(countClose % 2 != 0){
+                ans += (countClose + 1) / 2;
+            }
+        }
+
+        return ans; 
+    }
+}
