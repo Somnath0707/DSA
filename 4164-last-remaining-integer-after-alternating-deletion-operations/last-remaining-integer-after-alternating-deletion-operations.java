b6ba650a7834502993d53bf7e0defe8c01a2640e
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-06-26 22:05:37 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (82.05%) - LeetSync

--- 4164-last-remaining-integer-after-alternating-deletion-operations/last-remaining-integer-after-alternating-deletion-operations.java
+++ 4164-last-remaining-integer-after-alternating-deletion-operations/last-remaining-integer-after-alternating-deletion-operations.java
@@ -1,53 +1,48 @@
 class Solution {
     public long lastInteger(long n) {
-        long num = n; 
-        if(n % 2 == 0) num = n/2; 
-        else num = n/2 + 1; 
-        
+        long num = n ; 
+        if(n % 2 == 0 ) num = n/2; 
+        else num = n/2 + 1 ; 
+
         long temp = num; 
 
-        long side = 0; 
-        long left = 0; 
-        long right = 0; 
-        
+        long side = 1 ; 
+        long left = 0 ; 
+        long right = 0 ; 
+        long size = 1; 
 
-        long skipSize = 1; 
+        while( num != 1 ) {
+            // if curr is odd 
+            if(num % 2 != 0 ){
+                num = num / 2 + 1 ; 
+                if(num == 1 ) break; 
+                
+            }
+            else if(num % 2 == 0 ) {
+                num = num/2 ; 
+                if(side == 0 ) {
+                    right += size; 
+                }else {
+                    left += size; 
+                }
+                if(num == 1 ) break;
+            }
 
-        while(num != 1) {
-            if(num % 2 != 0) {
-                num = num / 2 + 1; 
-                if(num == 1) break; 
-                
-                if(side == 1) side = 0; 
-                else side = 1; 
-            }
-            else if(num % 2 == 0) { 
-                num = num / 2; 
-                
-                if(side == 0) {
-                    left += skipSize; 
-                }
-                else {
-                    right += skipSize; 
-                }
-                if(num == 1) break; 
-                if(side == 0) side = 1; 
-                else side = 0; 
-            }
-            skipSize *= 2; 
+            if(side == 1)
+                side = 0 ; 
+                else side = 1 ; 
+
+            size *= 2; 
         }
-        
-        long ans = 0; 
-        
-        if(side == 1) {
-            long number = temp - right; 
-            ans = 2 * number - 1;
+        long ans = 0 ; 
+        if(side == 1 ){
+            long number = temp-right; 
+            ans =  2 * number - 1;
         }
-        if(side == 0) { 
-            long number = left + 1;
-            ans = 2 * number - 1; 
+        if(side == 0 ) { 
+            long number = left+1;
+            ans =  2 * number - 1 ; 
         }
-        
         return ans; 
     }
 }
