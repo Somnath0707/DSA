de704d39ab2fb19f972d185736391373baf825cf
Somnath Ghorpade <149756531+Somnath0707@users.noreply.github.com>
2026-06-26 14:20:09 +0530

    Time: 107 ms (16.29%) | Memory: 46.5 MB (18.89%) - LeetSync

--- 959-3sum-with-multiplicity/3sum-with-multiplicity.java
+++ 959-3sum-with-multiplicity/3sum-with-multiplicity.java
@@ -1,26 +1,32 @@
 class Solution {
     int MOD = 1000000007;
     public int threeSumMulti(int[] arr, int target) {
-        Map<Integer,Integer> map = new HashMap<>(); 
+        // 1. Array instead of Map. Size 101 because arr[i] is 0 to 100
+        int[] count = new int[101]; 
         for(int n : arr){
-            map.put(n , map.getOrDefault(n , 0 ) + 1 ) ; 
+            count[n]++; 
         }
-        int n = arr.length ; 
-        int ans = 0 ; 
-        for(int i = 0; i < n-2 ; i++){
+        
+        int n = arr.length; 
+        int ans = 0; 
+        
+        for(int i = 0; i < n - 2; i++){
             int num = arr[i]; 
-            map.put(num , map.getOrDefault(num , 0 ) -1 ) ;
-            if(map.get(num) == 0 ) map.remove(num);
-            Map<Integer,Integer> temp = new HashMap<>(map); 
-            for(int j = i+1 ; j < n ; j++){
+            count[num]--; // Remove arr[i] from available pool
+            
+            // 2. Cloning a 101-length primitive array takes literal nanoseconds
+            int[] temp = count.clone(); 
+            
+            for(int j = i + 1; j < n; j++){
                 int find = target - (num + arr[j]); 
-                temp.put(arr[j] , temp.getOrDefault(arr[j] , 0 ) -1 );
-                if(temp.get(arr[j]) == 0 ) temp.remove(arr[j]);
-                if(temp.containsKey(find)){
-                    ans = (ans +  temp.get(find)) % MOD;
+                
+                temp[arr[j]]--; // Remove arr[j] before checking
+                
+                // 3. Since 'find' could be up to 300, just make sure it's safely between 0 and 100
+                if(find >= 0 && find <= 100){
+                    ans = (ans + temp[find]) % MOD;
                 }
             }
-            
         }
 
         return ans; 
