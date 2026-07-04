566e3cf03cf2efc71f09b5a0de144b02bfe2d574
Som <149756531+Somnath0707@users.noreply.github.com>
2026-07-04 11:41:14 +0530

    Time: 95 ms (22.80%) | Memory: 127.6 MB (60.10%) - LeetSync

--- 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.java
+++ 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.java
@@ -1,32 +1,41 @@
 class Solution {
-    public int longestEqualSubarray(List<Integer> list, int k) {
-        int left =0 ; 
+    public int longestEqualSubarray(List<Integer> nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+
         int max = 0 ; 
-        int n = list.size() ; 
-        Map<Integer , Integer> map = new HashMap<>();
-
+        int nonMax = 0 ; 
+        int maxAll = 0 ; 
+        int j = 0 ; 
+        int n = nums.size();
         for(int i = 0 ; i < n ; i++){
-            int val = list.get(i);
-
-            map.put(val , map.getOrDefault(val , 0 ) + 1 ) ; 
             
-            max = Math.max(max , map.get(val));
+            while(j < n ){
+                int val = nums.get(j);
 
-            // System.out.println("right " + i + " left " + left + " " + 1 + " MAX " + max );
+                map.put(val , map.getOrDefault(val , 0) + 1 ) ; 
+                max = Math.max(map.get(val) , max );
 
+                
 
-            if((i - left + 1) - max > k ){
-                // System.out.println("Inside Start");
-                // System.out.println("right " + i + " left " + left + " " + 1 + " MAX " + max );
-                int leftEle = list.get(left);
+                nonMax = j-i+1 - max; 
+                // System.out.println(val  + " " + max + " " + j + " " + i  + " " + nonMax);
+                j++; 
 
-                map.put(leftEle, map.get(leftEle) - 1);
-
-                left ++ ; 
-                // System.out.println("Inside End Removed : " + leftEle);
+                if(nonMax > k) break;
             }
+            maxAll = Math.max(max , maxAll);
+            // System.out.println("Oustside");
+            //  System.out.println(nums.get(i) +  " " + max + " " + j + " " + i  + " " + nonMax);
+            
+            map.put(nums.get(i) , map.get(nums.get(i)) - 1);
+
+            // if(j == n-1)break;
+
         }
 
-        return max; 
+        return maxAll; 
     }
 }
+
+
+// 4 4 4 4 2 3 2 6 k = 2
