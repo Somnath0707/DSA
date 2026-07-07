18198ed08dc7ff446d0b76922082f453fc8e57ec
Som <149756531+Somnath0707@users.noreply.github.com>
2026-07-07 20:34:07 +0530

    Time: 66 ms (29.70%) | Memory: 48.2 MB (11.75%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.java
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.java
@@ -1,5 +1,6 @@
 class Solution {
     public String repeatLimitedString(String s, int repeatLimit) {
+        // return "";
         Map<Character , Integer> map = new HashMap<>(); 
 
         for(char ch : s.toCharArray()){
@@ -12,30 +13,32 @@ class Solution {
             pq.offer(new int[]{entry.getKey() , entry.getValue()});
         }
         StringBuilder str = new StringBuilder(); 
-        
+        char prev = '#';
         while(!pq.isEmpty()){
             int temp[] = pq.poll(); 
             char ch = (char)temp[0];
             int val = temp[1]; 
-            
             int curr = Math.min(val , repeatLimit);
+            if(prev == ch) curr-=1 ; 
             for(int i = 0 ; i < curr ; i++){
                 str.append(ch);
             }
-            
             int newVal = val - curr; 
-            if (newVal > 0) {
-                if(pq.isEmpty()) break; 
-                
-                int temp2[] = pq.poll();
-                char ch2 = (char)temp2[0]; 
-                int val2 = temp2[1]; 
-                str.append(ch2);
+            if(prev == ch) newVal+=1 ; 
+            if(newVal > 0 ){
+            if(pq.isEmpty())break; 
+            int temp2[] = pq.poll();
+            char ch2 = (char)temp2[0]; 
+            int val2 = temp2[1]; 
+            str.append(ch2);
 
-                int newVal2 = val2 - 1; 
-                if(newVal2 != 0 ) pq.offer(new int[]{ch2 , newVal2});
-                pq.offer(new int[]{ch , newVal});
+            int newVal2 = val2-1; 
+            prev = ch2; 
+            if(newVal != 0 ) pq.offer(new int[]{ch , newVal});
+            if(newVal2 != 0 ) pq.offer(new int[]{ch2 , newVal2});
             }
+            else 
+            prev = ch;
         }
 
         return str.toString(); 
