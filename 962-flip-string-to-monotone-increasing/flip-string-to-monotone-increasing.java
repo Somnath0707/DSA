7a76efc70055fea82ffecc2e5bf1b7723749fa98
Som <149756531+Somnath0707@users.noreply.github.com>
2026-07-14 20:02:35 +0530

    Time: 10 ms (51.39%) | Memory: 47.2 MB (25.28%) - LeetSync

--- 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.java
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.java
@@ -5,16 +5,15 @@ class Solution {
         int n =s.length();
         boolean startCount = false;
         for(int i = 0 ; i < n ; i++){
-            if(s.charAt(i) == '1' && startCount == false ){
+            if(s.charAt(i) == '1' ){
                 startCount = true; 
                 one++; 
             }
             else if(startCount){
-                if(s.charAt(i) =='1') one++; 
-                else{
-                    zero++; 
-                    zero = Math.min(zero , one);
+                if(s.charAt(i) =='0'){ zero++; 
+                zero = Math.min(zero , one);
                 }
+                else one++; 
             }
         }
         return zero;
