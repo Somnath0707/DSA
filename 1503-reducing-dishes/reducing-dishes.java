0aa46c6132f51d2662100ecc4f53e425a5f8a049
Som <149756531+Somnath0707@users.noreply.github.com>
2026-07-21 19:51:11 +0530

    Time: 167 ms (5.22%) | Memory: 106.6 MB (5.60%) - LeetSync

---
+++ 1503-reducing-dishes/reducing-dishes.java
@@ -0,0 +1,29 @@
+class Solution {
+    Map<String , Integer> dp; 
+    public int f(int i , int val ,  int nums[]){
+        int n = nums.length; 
+        if( i == n) return 0 ; 
+
+        String key = i + "#" + val; 
+
+        if(dp.containsKey(key)) return dp.get(key); 
+
+        int take = 0 ; 
+
+        if(nums[i] < 0){
+            take = Math.max(nums[i] * val + f(i+1 , val+1 , nums) , f(i+1 , val , nums));
+        }
+        else {
+            take = Math.max(take , nums[i] * val + f(i+1 , val+1 , nums));  
+        }
+
+        dp.put(key , take );
+        return take ;
+    }
+    public int maxSatisfaction(int[] nums) {
+      // a chef has collected date on the satisfaction leve of his n dish chef can cook any dish in 1   
+      Arrays.sort(nums); 
+      dp = new HashMap<>(); 
+      return f(0 , 1, nums);
+    }
+}
