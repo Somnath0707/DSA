06119900b4c239798d3a1f2779ce8c90efec8b8b
Som <149756531+Somnath0707@users.noreply.github.com>
2026-07-24 15:34:14 +0530

    Time: 3 ms (40.00%) | Memory: 46.4 MB (24.00%) - LeetSync

--- 1251-longest-chunked-palindrome-decomposition/longest-chunked-palindrome-decomposition.java
+++ 1251-longest-chunked-palindrome-decomposition/longest-chunked-palindrome-decomposition.java
@@ -1,73 +1,34 @@
-class RollingHash {
-    long pow[];
-    long hash[];
-    long base = 31;
-    long MOD = 1000000007;
-
-    RollingHash(String s) {
-        int n = s.length();
-        pow = new long[n + 1];
-        hash = new long[n];
-
-        pow[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            pow[i] = (pow[i - 1] * base) % MOD;
-        }
-
-        hash[0] = s.charAt(0) - 'a' + 1;
-        for (int i = 1; i < n; i++) {
-            hash[i] = (hash[i - 1] * base + (s.charAt(i) - 'a' + 1)) % MOD;
-        }
-    }
-
-    long getHash(int l, int r) {
-        if (l == 0)
-            return hash[r];
-
-        long ans = (hash[r] - hash[l - 1] * pow[r - l + 1]) % MOD;
-
-        if (ans < 0)
-            ans += MOD;
-        return ans;
-    }
-}
-
 class Solution {
     public int longestDecomposition(String s) {
-        int n = s.length();
-        int left = 0;
-        int right = n - 1;
-        RollingHash re = new RollingHash(s);
-        int ans = 0;
+        
 
-        while (left <= right) {
-            // int i = left ; 
-            // int j = right ; 
-            boolean found = false;
-            for (int i = left; i <= right; i++) {
-                int len = i - left + 1;
-                int leftLen = right - len + 1;
-                if (re.getHash(left, i) == re.getHash(leftLen, right)) {
-                    // ans+=2;
-                    if (left == leftLen) {
-                        // The prefix and suffix are actually the SAME substring.
-                        ans += 1;
-                    } else {
-                        ans += 2;
-                    }
-                    len = i - left + 1;
-                    right = right - len;
-                    left = i + 1;
-                    found = true;
+        int left =0 ; 
+        int right = s.length()-1; 
+        int ans = 0 ;
+
+        while(left <= right){
+            boolean found = false; 
+            for(int i = left ; i <= right ; i++){
+                String leftSubstring = s.substring(left , i+1); 
+                int len = i - left+1; 
+                String rightSubString = s.substring(right - len +1, right+1);
+
+                if(leftSubstring.equals(rightSubString)){
+                    if(i == right){
+                        ans+=1; 
+                    }else ans+=2; 
+                    left = i+1;
+                    right -= len ;  
+                    found = true; 
                     break;
                 }
             }
-            if (!found) {
-                ans += 1;
-                break;
-            }
+            // break;
+            if(!found) break;
+
         }
 
-        return ans;
+        if(ans ==0) return 1; 
+        return ans; 
     }
 }
