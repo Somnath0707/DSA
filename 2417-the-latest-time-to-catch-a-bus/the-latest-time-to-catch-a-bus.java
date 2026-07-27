5209c5e2ebbf86c7301830581ba707ce2e41e057
Som <149756531+Somnath0707@users.noreply.github.com>
2026-07-27 14:54:43 +0530

    Time: 84 ms (5.81%) | Memory: 112.9 MB (15.70%) - LeetSync

--- 2417-the-latest-time-to-catch-a-bus/the-latest-time-to-catch-a-bus.java
+++ 2417-the-latest-time-to-catch-a-bus/the-latest-time-to-catch-a-bus.java
@@ -1,65 +1,71 @@
 class Solution {
-    public boolean  check(int []nums , int arr[] , int cap , int mid){
+    public boolean check(int[] nums, int[] arr, int cap, int mid) {
         int count = 0; 
-        int i = 0 ; 
-        int j = 0 ; 
+        int i = 0; 
+        int j = 0; 
 
-        while(i < nums.length && j < arr.length){
-            if(mid <= arr[j] && mid <= nums[i] ){
+        while (i < nums.length && j < arr.length) {
+            if (mid <= arr[j] && mid <= nums[i]) {
                 return true; 
             }
-            else if(arr[j] <= nums[i]){
+            else if (arr[j] <= nums[i]) {
                 j++; 
                 count++; 
             }
-            // else if(nums[i] < arr[j]){
-            //     i++;
-            //     count = 0; 
-            // }
             else {
                 i++;
-                // j++; 
-                count = 0 ; 
+                count = 0; 
             }
-            if(count == cap ){
-                count = 0 ; 
+            
+            if (count == cap) {
+                count = 0; 
                 i++; 
             }
         }
+        
         while (i < nums.length) {
-            if(mid <= nums[i])
-            return true;
+            if (mid <= nums[i]) {
+                return true;
+            }
             i++;
         }
+        
         return false; 
     }
+
     public int latestTimeCatchTheBus(int[] buses, int[] passengers, int capacity) {
         Arrays.sort(buses); 
         Arrays.sort(passengers);
 
         int left = 1; 
         int right = buses[buses.length - 1]; 
-        int ans = 0 ; 
+        int ans = 0; 
 
         Set<Integer> set = new HashSet<>(); 
-        for(int i =0 ; i < passengers.length ; i++){
-            set.add(passengers[i]);
-        }
-        while(left <= right){
-            int mid = left + (right - left ) / 2; 
-
-            if(check(buses , passengers , capacity , mid)){
-                ans = mid; 
-                left = mid+1 ; 
-            }
-            else{
-                right = mid-1; 
-            }
+        for (int p : passengers) {
+            set.add(p);
         }
 
-        while(set.contains(ans)){
-            ans--; 
+        while (left <= right) {
+            int mid = left + (right - left) / 2; 
+            int currMid = mid;
+
+            while (set.contains(currMid) && currMid < right) {
+                currMid++;
+            }
+
+            while (set.contains(currMid) && currMid > left) {
+                currMid--;
+            }
+
+            if (!set.contains(currMid) && check(buses, passengers, capacity, currMid)) {
+                ans = Math.max(ans, currMid);
+                left = mid + 1;
+            } else {
+                right = mid - 1; 
+            }
         }
+
         return ans; 
     }
 }
