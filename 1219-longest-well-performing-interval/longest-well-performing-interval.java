724985577c00910c1c7cf4fd5e62a38b3ecbc97c
Som <149756531+Somnath0707@users.noreply.github.com>
2026-07-29 11:58:05 +0530

    Time: 12 ms (72.58%) | Memory: 47 MB (88.45%) - LeetSync

--- 1219-longest-well-performing-interval/longest-well-performing-interval.java
+++ 1219-longest-well-performing-interval/longest-well-performing-interval.java
@@ -1,47 +1,30 @@
 class Solution {
     public int longestWPI(int[] hours) {
-        // 9 9 6 0 6 6 9
-
-        // int left = ; 
-        // int right =hours.length-1; 
-        // int ans = -1; 
-        // while(left < right){
-        //     int mid = left + (right -left) / 2; 
-
-        //     if(check(mid , hours)){
-        //         ans = mid ; 
-        //         left = mid+1; 
-        //     }
-        //     else 
-        // }
         int n = hours.length; 
-        int ans = 0 ; 
-        for(int i = 0 ; i < n ; i++){
-            int countGreat = 0;
-            int countLow = 0 ;
-            for(int j = i ; j < n ; j++){
-                if(hours[j] > 8) countGreat++; 
-                else countLow++; 
+        int prefixSum[] = new int[n+1]; 
 
-                // System.out.print(ln)
-                if(countGreat > countLow){
-                    ans = Math.max(ans , j-i+1); 
-                }
+        prefixSum[0] = 0 ; 
+        int ind = 0 ; 
+        for(int i = 1 ; i <= n ;i++){
+            prefixSum[i] = prefixSum[i-1] + (hours[i-1] > 8 ? 1 : -1);
+        }
+
+        Stack<Integer> st = new Stack(); 
+
+        for(int i = 0 ; i <= n ; i++){
+            if(st.isEmpty() || prefixSum[st.peek()] > prefixSum[i]){
+                st.push(i); 
+            }
+        }
+
+        int ans = 0 ;
+        for(int i = n ; i >= 0 ; i--){
+            while(!st.isEmpty() && prefixSum[st.peek()] < prefixSum[i]){
+                ans = Math.max( ans , i - st.peek());
+                st.pop(); 
             }
         }
 
         return ans; 
-
     }
 }
-
-// is this condtion monotonic ? [9,9,6,0,6,6,9]
-// means if there is well perfoming days of 3 is it sure that there are well performing days of 2 and 1 too in this it is there 
-
-// 9 2 2 9 9 2 2 2
-// so the condition is not monotonic so no binary search on window size 
-
-
-
-
-
