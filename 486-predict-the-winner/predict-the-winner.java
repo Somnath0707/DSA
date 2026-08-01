d9cafb6be09958df7a6b104048c54bbfd3f7846a
Som <149756531+Somnath0707@users.noreply.github.com>
2026-08-01 08:59:21 +0530

    Time: 35 ms (31.10%) | Memory: 53 MB (5.44%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.java
@@ -0,0 +1,32 @@
+class Solution {
+    Map<String , Boolean > dp ; 
+    public boolean f(int i , int j , int sum1 , int sum2 , int who , int [] nums){
+        if(i > j){
+            if(sum1 >=sum2)return true; 
+            return false; 
+        }
+        String key = i + "#" + j + "#" + sum1 + "#" + sum2 + "#" + who ; 
+        if(dp.containsKey(key)) return dp.get(key); 
+        boolean  take = false; 
+        if(who == 0 ){
+            int newSum1 = sum1 + nums[i];
+            int newSum2 = sum1 + nums[j]; 
+            take = take || f(i+1 , j , newSum1 , sum2 , 1 , nums); 
+            take = take || f(i , j-1 , newSum2 , sum2 , 1 , nums); 
+        }
+        else{
+            int newSum = sum2 + nums[i]; 
+            int newSum1 = sum2 + nums[j]; 
+            take = f(i+1 , j ,  sum1 , newSum , 0 , nums) && f(i , j-1 , sum1 , newSum1 , 0 , nums);
+        }
+        dp.put(key , take);
+        return take;  
+    }
+    public boolean predictTheWinner(int[] nums) {
+        // so we can do is like take 
+        // so if i == j and sum1 > sum2 return true; return false 
+        int n = nums.length; 
+        dp = new HashMap<>(); 
+        return f(0 , n-1 , 0 , 0 , 0 , nums); 
+    }
+}
