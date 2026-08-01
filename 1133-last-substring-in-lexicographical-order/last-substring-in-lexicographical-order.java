0e4df69e4c3512a1ba9fd129d2a5a4588e7af0e1
Som <149756531+Somnath0707@users.noreply.github.com>
2026-08-01 12:27:49 +0530

    Time: 347 ms (5.48%) | Memory: 67.3 MB (5.48%) - LeetSync

--- 1133-last-substring-in-lexicographical-order/last-substring-in-lexicographical-order.java
+++ 1133-last-substring-in-lexicographical-order/last-substring-in-lexicographical-order.java
@@ -1,92 +1,123 @@
-class RollingHash {
-    long[] pow;
-    long[] hash;
-    long base = 6767;
+class RollingHash{
+    long pow[]; 
+    long base = 6767; 
+    long hash[]; 
     long MOD = 1000000007L;
 
-    RollingHash(String s) {
-        int n = s.length();
+    RollingHash(String s ){
+        int n = s.length(); 
 
-        pow = new long[n + 1];
-        hash = new long[n + 1];
+        pow = new long[n+1]; 
+        hash = new long[n+1]; 
 
-        pow[0] = 1;
-
-        for (int i = 1; i <= n; i++) {
-            pow[i] = (pow[i - 1] * base) % MOD;
+        pow[0] = 1 ; 
+        for(int i = 1 ; i <= n ; i++){
+            pow[i] = (pow[i-1] * base) % MOD ; 
         }
 
-        for (int i = 0; i < n; i++) {
-            hash[i + 1] = (hash[i] * base + s.charAt(i)) % MOD;
+        for(int i = 0 ; i < n ; i++){
+            int x = s.charAt(i); 
+            hash[i+1] = (hash[i] * base + x) % MOD; 
         }
     }
 
-    long getHash(int l, int r) {
-        long ans = hash[r + 1] - (hash[l] * pow[r - l + 1]) % MOD;
-        if (ans < 0) ans += MOD;
-        return ans;
+    long getHash(int l , int r){
+        long ans = hash[r+1] - (hash[l] * pow[r-l+1]) % MOD; 
+        if(ans < 0 ) ans+= MOD ; 
+        return ans; 
     }
 }
-
 class Solution {
+    RollingHash re;
+    public  boolean check(int first , int second , String s){
+        int n = s.length(); 
 
-    RollingHash rh;
+       
 
-    boolean check(int first, int second, String s) {
 
-        int n = s.length();
+        int left = 0 ; 
+        int len = Math.min(n-first , n-second);
+        int right = len; 
+        int ans = -1 ; 
 
-        int len = Math.min(n - first, n - second);
+        while(left <= right){
+            int mid = left + (right - left) / 2; 
 
-        int low = 0;
-        int high = len;
-
-        // Longest Common Prefix
-        while (low < high) {
-            int mid = (low + high + 1) / 2;
-
-            if (rh.getHash(first, first + mid - 1) ==
-                rh.getHash(second, second + mid - 1)) {
-
-                low = mid;
-            } else {
-                high = mid - 1;
+            if(re.getHash(first, first +mid-1) == re.getHash(second , second + mid -1)){
+                left = mid+1; 
+            }else{
+                ans = mid; 
+                right = mid-1; 
             }
         }
-
-        int lcp = low;
-
-        if (lcp == len) {
-            return (n - first) < (n - second);
+        if(ans == -1){
+            if(len == n-second) return false;
+            return true; 
         }
+        else{
+            if(s.charAt(first+ans-1) > s.charAt(second + ans -1)) return false; 
+            return true;  
+        }
+        
 
-        return s.charAt(first + lcp) < s.charAt(second + lcp);
     }
-
     public String lastSubstring(String s) {
+        re = new RollingHash(s);
+        // brute force 
+        // TreeSet<String> set = new TreeSet<>(); 
+        // int n = s.length(); 
+        // for(int i = 0 ; i < n ; i++){
+        //     for(int j = i ; j < n ; j++){
+        //         set.add(s.substring(i , j+1));
+        //     }
+        // }
+        // String ans = set.last();
+        // return ans; 
 
-        rh = new RollingHash(s);
+        // // But got mle not even tle cause may be we cant store this much 
+        // l le lee leet leetc leetco leetcod leetcode
+        // e ee eet eetc eetco eetcod eetcode
+        // e et etc etco etcod etcode 
+        // t tc tco tcod tcode 
+        // c co cod code 
+        // o od ode
+        // d de
+        // e
 
+        // a ab aba abab 
+        // b ba bab 
+        // a ab 
+        // b
+        
+
+
+        // c ca cac caca cacac cacacb
+        // a ac aca acac acab
+        // c ca cac cacb
+        // a ac acb
+        // c cb 
+        // b
         char prev = '<';
-        int max = -1;
-
-        int n = s.length();
-
-        for (int i = 0; i < n; i++) {
-
-            char ch = s.charAt(i);
-
-            if (ch > prev) {
-                prev = ch;
-                max = i;
-            } else if (ch == prev) {
-
-                if (!check(i, max, s)) {
-                    max = i;
+        int max = -1; 
+        int n = s.length(); 
+        for(int i = 0 ; i < n ; i++){
+            char ch = s.charAt(i); 
+            if(ch > prev ){
+                max = i; 
+                prev = ch; 
+            }
+            else if(ch == prev ){
+                //curr is greter it return false; 
+                if(check(i , max , s)){
+                    continue; 
+                }
+                else {
+                    prev = ch ; 
+                    max = i ; 
                 }
             }
         }
 
-        return s.substring(max);
+        return s.substring(max , s.length()); 
     }
 }
