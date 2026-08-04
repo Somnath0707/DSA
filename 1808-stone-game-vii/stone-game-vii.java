42928249ede19d03697c0323ce47a139dd125be7
Som <149756531+Somnath0707@users.noreply.github.com>
2026-08-04 08:33:33 +0530

    Time: 91 ms (39.72%) | Memory: 183.9 MB (9.93%) - LeetSync

---
+++ 1808-stone-game-vii/stone-game-vii.java
@@ -0,0 +1,35 @@
+class Solution {
+    Integer[][] dp ; 
+    public int f(int i , int j , int[]nums){
+        if(i > j) return 0 ; 
+        if(dp[i][j] != null ) return dp[i][j]; 
+        int take = Integer.MIN_VALUE; 
+
+
+        int take1 = nums[j] - nums[i] - f(i+1 , j , nums);
+        int take2 = nums[j-1] - nums[i-1] - f(i , j-1 , nums); 
+
+        take = Math.max(take , Math.max(take1 , take2));
+
+        return dp[i][j] = take; 
+    }
+    public int stoneGameVII(int[] stones) {
+        // take prefix sum 
+        // alice do Max(left ,right)-bob(max(leftright)) at end reutnr the diff 
+        
+        int n = stones.length ; 
+        dp = new Integer[n+2][n+2]; 
+        int nums[] = new int[n+1]; 
+
+        nums[0] = stones[0]; 
+
+        for(int i = 1 ; i < n+1 ; i++){
+            nums[i] = stones[i-1] + nums[i-1]; 
+        }
+        // 0 5 8 9 13 15
+        int m = nums.length-1; 
+
+        return f(1 , m  ,nums);
+
+    }
+}
