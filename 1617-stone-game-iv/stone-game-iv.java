7bdbed702d5060a9848b76267fb0d57e0ce971fe
Som <149756531+Somnath0707@users.noreply.github.com>
2026-08-10 09:00:16 +0530

    Time: 212 ms (5.22%) | Memory: 58.6 MB (5.48%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -1,19 +1,33 @@
 class Solution {
-    Boolean dp[]; 
-    public boolean f(int n ){
-        if(n == 0 ) return false ; 
-        if(dp[n] != null) return dp[n];
-        for(int i = 1 ; i*i <= n ; i++){
-            if(!f(n- i*i)){
-                return dp[n] = true;
-            }
+    Boolean dp[][]; 
+
+    public boolean  f(int n , int turn ){
+        if(n == 0){
+            if(turn == 1) return true; 
+            return false; 
         }
-        return dp[n] = false;
+        if(dp[n][turn]!= null) return dp[n][turn]; 
+        boolean alice = false; 
+        if(turn == 0){
+        for(int i = 1 ; i *i <= n ; i++){
+            if(n-i*i >= 0){
+                alice = alice || f(n-i*i , 1); 
+            }
+        }}
+
+        if(turn == 1){
+            alice = true;
+            for(int i = 1 ; i *i <= n ; i++){
+                
+                if(n-i*i >= 0){
+                    alice = alice && f(n-i*i , 0); 
+                }
+            }  
+        }
+        return dp[n][turn] =  alice ; 
     }
     public boolean winnerSquareGame(int n) {
-        // pick the squares until the i^2 < n 
-        // check that if there is any chance of the bob winning 
-        dp = new Boolean[n+1];
-        return f(n);
+        dp = new Boolean[n+1][3]; 
+        return f(n , 0 ); 
     }
 }
