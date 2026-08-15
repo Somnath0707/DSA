de008c15de01609a8a654fd0b2b7e38e90c50dd3
Som <149756531+Somnath0707@users.noreply.github.com>
2026-08-15 09:16:26 +0530

    Time: 2 ms (68.38%) | Memory: 133.3 MB (75.00%) - LeetSync

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.java
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.java
@@ -1,67 +1,17 @@
 class Solution {
-
-    int solve(int[] nums, int idx, int ans, int XOR, int[] DP) {
-
-        if (idx >= nums.length) {
-            return ans;
-        }
-
-        if (DP[idx] != -1) {
-            return DP[idx];
-        }
-
-        int take = Integer.MIN_VALUE;
-
-        if ((XOR ^ nums[idx]) != 0) {
-            take = solve(
-                nums,
-                idx + 1,
-                ans + 1,
-                XOR ^ nums[idx],
-                DP
-            );
-        }
-
-        int ignore = solve(
-            nums,
-            idx + 1,
-            ans,
-            XOR,
-            DP
-        );
-
-        return DP[idx] = Math.max(take, ignore);
-    }
-
     public int longestSubsequence(int[] nums) {
-        if(nums.length == 9 && nums[2] == 7 && nums[4] ==0 ) return 8 ; 
-        int XOR = 0;
-        boolean same = true;
-        int c = nums[0];
+        int xor = 0 ;
+        boolean allZero = true ; 
 
-        for (int i : nums) {
-            XOR ^= i;
-
-            if (same && i != c) {
-                same = false;
-            }
+        for(int i : nums){
+            xor ^= i; 
+            if(i != 0) allZero = false; 
         }
 
-        if (XOR != 0) {
-            return nums.length;
-        }
+        if(allZero) return 0; 
 
-        if (same && nums.length % 2 == 0) {
-            if (c == 0) {
-                return 0;
-            }
+        if(xor!=0) return nums.length ; 
+        return nums.length -1; 
 
-            return nums.length - 1;
-        }
-
-        int[] DP = new int[nums.length];
-        Arrays.fill(DP, -1);
-
-        return solve(nums, 0, 0, 0, DP);
     }
 }
