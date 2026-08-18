75c13f94318259bd09f561dfa5f2b32a9fc0b415
Som <149756531+Somnath0707@users.noreply.github.com>
2026-08-18 09:29:31 +0530

    Time: 4 ms, Space: 46.8 MB - AlgoVault

--- leetcode/easy/3471-find-the-largest-almost-missing-integer/solution.java
+++ leetcode/easy/3471-find-the-largest-almost-missing-integer/solution.java
@@ -2,26 +2,37 @@ class Solution {
     public int largestInteger(int[] nums, int k) {
         int ans = -1 ; 
         int n = nums.length ;
+        int temp = -1 ; 
+       
 
-        Map<Integer,Integer> map = new HashMap<>(); 
         
-        for(int i = 0 ; i < n-k+1 ; i++){
-            Set<Integer> taken = new HashSet<>(); 
-            for(int j = i ; j < i+k ; j++){
-                if(taken.contains(nums[j])) continue; 
-                int val = nums[j]; 
-                map.put(val , map.getOrDefault(val , 0)+1);
-                taken.add(val); 
-            }
-        }
+        Map<Integer,Integer> map = new HashMap<>(); 
+        int j = 0 ; 
 
-        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-            int val = entry.getValue(); 
-            int key = entry.getKey(); 
-            if(val == 1){
-                ans = Math.max(ans , key); 
+        for(int i = 0 ; i < n-k+1 ; i++){
+            Set<Integer>set = new HashSet<>();
+            j = i ;  
+            while(j-i < k){
+                if(set.contains(nums[j])){
+                    j++; 
+                }else{
+                    map.put(nums[j] , map.getOrDefault(nums[j] , 0)+1);
+                    set.add(nums[j]);
+                    j++;
+                }
             }
         }
-        return ans; 
+         for(int num : nums){
+            if(map.get(num) <= 1)
+            temp = Math.max(temp ,num); 
+        }
+        if(k == 1 || k == n) return temp; 
+        int first = nums[0]; 
+        int last = nums[n-1]; 
+        if(map.get(first) > 1 && map.get(last) > 1) return -1; 
+        else if(map.get(first)>1) return last; 
+        else if(map.get(last) > 1) return first; 
+        else
+        return Math.max(first, last); 
     }
 }
