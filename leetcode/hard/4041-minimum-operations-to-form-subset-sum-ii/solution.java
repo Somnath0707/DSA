63ff5cf385d25bae4b37b896e424930b04c24e17
Som <149756531+Somnath0707@users.noreply.github.com>
2026-08-30 09:33:51 +0530

    4041. Minimum Operations to Form Subset Sum II: Time: 1780 ms, Space: 48.1 MB - AlgoVault (+2 files) - AlgoVault

---
+++ leetcode/hard/4041-minimum-operations-to-form-subset-sum-ii/solution.java
@@ -0,0 +1,84 @@
+class Solution {
+    Integer dp[][] ; 
+    public int f(int i, int total, int nums[], int sum) {
+
+        if(total == sum) return 0 ; 
+        if (i == nums.length){
+            return 1000000; 
+        }
+
+         if (total > sum)
+            return 1000000;
+
+
+        if(dp[i][total] != null) return dp[i][total]; 
+        int take = 1000000;
+        int skip = 1000000;
+       
+        take = f(i + 1, total + nums[i], nums, sum);
+
+        if (total + nums[i] <= sum)
+            take = f(i + 1, total + nums[i], nums, sum);
+
+        int currSum = nums[i];
+        int op = 0;
+
+        while (currSum != 0) {
+            currSum /= 2;
+            op++;
+
+            if (total + currSum <= sum)
+                take = Math.min(take,
+                        op + f(i + 1, total + currSum, nums, sum));
+
+            int mul = currSum;
+            int mulOp = op;
+
+            while (mul > 0 && mul <= sum) {
+                mul *= 2;
+                mulOp++;
+
+                if (mul > sum)
+                    break;
+
+                if (total + mul <= sum)
+                    take = Math.min(take,
+                            mulOp + f(i + 1, total + mul, nums, sum));
+            }
+        }
+        currSum = nums[i];
+        op = 0;
+        while (currSum <= sum) {
+            currSum *= 2;
+            op++;
+            if (currSum > sum)
+                break;
+            take = Math.min(take, op + f(i + 1, total + currSum, nums, sum));
+
+            int div = currSum;
+            int divOp = op;
+
+            while (div != 0) {
+                div /= 2;
+                divOp++;
+                if (total + div <= sum) {
+                    take = Math.min(take, divOp + f(i + 1, total + div, nums, sum));
+                }
+            }
+        }
+
+        skip = f(i + 1, total, nums, sum);
+
+
+        return dp[i][total] =  Math.min(take , skip); 
+    }
+
+    public int minOperations(int[] nums, int sum) {
+        int n = nums.length; 
+        dp = new Integer[n][sum+1]; 
+        int ans =  f(0, 0, nums, sum);
+
+        if(ans >= 1000000) return -1; 
+        return ans; 
+    }
+}
