e5ae96e0b349c1b6a068172f3ae46e64a5601f23
Som <149756531+Somnath0707@users.noreply.github.com>
2026-09-03 08:56:28 +0530

    3876. Construct Uniform Parity Array II: Time: 320 ms, Space: 201.9 MB - AlgoVault (+2 files) - AlgoVault

---
+++ leetcode/medium/3876-construct-uniform-parity-array-ii/solution.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean uniformArray(int[] nums) {
+        TreeMap<Integer,Integer> even = new TreeMap<>(); 
+        TreeMap<Integer,Integer> odd = new TreeMap<>();
+        int n = nums.length; 
+        for(int i = 0 ; i < nums.length ; i++){
+            int val = nums[i]; 
+            if(val % 2 == 0){
+                even.put(val , i);
+            }else{
+                odd.put(val , i);
+            }
+        }
+        boolean foundAns = true; 
+        for(int i = 0 ; i < n ; i++){
+            int val = nums[i]; 
+            if(val % 2 == 0) continue; 
+            else{
+                Integer key = odd.lowerKey(val);
+                if(key == null){
+                    foundAns = false; 
+                    break; 
+                }
+            }
+        }
+
+        if(foundAns) return foundAns; 
+        boolean foundAnsTwo = true; 
+        for(int i = 0 ; i < n ; i++){
+            int val = nums[i]; 
+            if(val % 2 != 0) continue; 
+            else{
+                Integer key = odd.lowerKey(val);
+                if(key == null){
+                    foundAnsTwo = false; 
+                    break; 
+                }
+            }
+        }
+        
+        return foundAns || foundAnsTwo; 
+    }
+}
